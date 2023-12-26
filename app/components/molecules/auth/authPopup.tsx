cdbd8145a493071eb9eba0913dd39eb386bcf542
rpc-vinay <131428813+rpc-vinay@users.noreply.github.com>
2023-12-26 19:46:26 +0530

    latest changes

--- app/components/molecules/auth/authPopup.tsx
+++ app/components/molecules/auth/authPopup.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { useDisclosure } from "@mantine/hooks";
-import { Modal, Button } from "@mantine/core";
+import { Modal } from "@mantine/core";
 import Form from "./form";
+import Button from '../../../elements/button';
 
 function AuthPopup() {
   const [opened, { open, close }] = useDisclosure(false);
@@ -22,12 +23,12 @@ function AuthPopup() {
       </Modal>
 
       <Button
-        onClick={open}
-        radius={"xl"}
-        className="login-btn text-[20px] font-semibold px-5 py-2 rounded-full text-[#0073C6] border-2 border-white underline "
-      >
-        Login & Signup
-      </Button>
+        key={"loginBtn"}
+        onChange={()=>open()}
+        buttonClass="login-btn text-[20px] font-semibold px-5 py-2 rounded-full text-[#0073C6] border-none underline bg-gradient-to-r from-[#EFF8FF] to-[#FFF] shadow-md"
+        title="Login/ Sign up"
+      />
+      
     </>
   );
 }
