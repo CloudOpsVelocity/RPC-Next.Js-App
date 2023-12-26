ccbe65c92f55e1beeb924509ec8ce685cba795f0
rpc-vinay <131428813+rpc-vinay@users.noreply.github.com>
2023-12-26 17:02:41 +0530

    adding new changes

--- app/components/layouts/primary/header.tsx
+++ app/components/layouts/primary/header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
   const [opened, { toggle }] = useDisclosure(false);
 
   return (
-    <header className="h-[90px] bg-transparent px-10 flex items-center justify-between fixed top-0 z-50 w-full">
+    <header className="h-[90px] bg-transparent px-10 flex items-center justify-between fixed top-0 z-50 w-full bg-white">
       <section>
         <Logo />
       </section>
@@ -14,10 +14,10 @@ const Header = () => {
         <Link className="text-xl font-medium mr-3 text-slate-700" href={"#"}>
           Blogs
         </Link>
-        <button className="text-[20px] font-semibold px-5 bg-[#1B6798] py-1.5 rounded-xl text-white">
+        <button className="text-[20px] font-semibold px-5 bg-[#227FBC] py-1.5 rounded-xl text-white">
           Post Listing
         </button>
-        <button className="login-btn text-[20px] font-semibold px-5 py-2 rounded-full text-[#0073C6] border-2 border-white underline ">
+        <button className="login-btn text-[20px] font-semibold px-5 py-2 rounded-full text-[#0073C6] border-2 border-white underline bg-gradient-to-r from-C5E7FF-500 to-F1F9FF-500 ">
           Login & Signup
         </button>
       </section>
