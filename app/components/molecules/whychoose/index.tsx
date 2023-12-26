8f5e6a7e8de8bb2af498fb0caa91627eea13bb84
rpc-vinay <131428813+rpc-vinay@users.noreply.github.com>
2023-12-26 11:52:09 +0530

    adding why choose code

--- app/components/molecules/whychoose/index.tsx
+++ app/components/molecules/whychoose/index.tsx
@@ -1,7 +1,101 @@
-import React from 'react'
-
-export default function WhyCHoose() {
+import React from "react";
+const WhyCHoose = () => {
   return (
-    <div>WhyCHooose</div>
-  )
-}
+    <div className="inline-flex flex-col items-start gap-[37px] relative top-[160px] left-[121px]">
+          <p className="relative w-fit mt-[-1.00px] [font-family:'Muna-Regular',Helvetica] font-normal text-transparent text-[64px] tracking-[2.56px] leading-[83.2px] whitespace-nowrap">
+            <span className="text-[#1f1f1f]">Why Choose</span>
+            <span className="[font-family:'Muna-Bold',Helvetica] font-bold text-[#138b16]"> Get Right Property?</span>
+          </p>
+          <div className="relative w-[1674px] h-[496px]">
+            <div className="h-[496px]">
+              <div className="relative w-[1674px] h-[496px]">
+                <div className="flex w-[1674px] h-[438px] items-center gap-[134px]">
+                  <p className="relative w-[584px] [font-family:'Montserrat-Medium',Helvetica] font-normal text-transparent text-[32px] tracking-[1.28px] leading-[32px]">
+                    <span className="font-medium text-[#767270] leading-[45.1px]">
+                      Choose us for real estate because we offer expert guidance, a vast property selection and
+                      commitment to your dreams.
+                      <br />
+                    </span>
+                    <span className="font-medium text-[#767270] leading-[19.5px]">
+                      {" "}
+                      <br />
+                    </span>
+                    <span className="font-medium text-[#a3aed0] leading-[19.5px]">
+                      <br />
+                    </span>
+                    <span className="[font-family:'Montserrat-SemiBold',Helvetica] font-semibold text-[#767270] leading-[33px]">
+                      Trust in our experience to find your perfect home!
+                    </span>
+                  </p>
+                </div>
+                <div className="absolute w-[957px] h-[496px] top-0 left-[712px]">
+                  <div className="inline-flex items-center gap-[44px] absolute top-0 left-[6px]">
+                    <div className="relative w-[287px] h-[377px]">
+                      <div className="relative w-[307px] h-[394px] top-[-6px] left-[-10px] bg-[100%_100%]">
+                        <div className="flex flex-col w-[245px] items-center justify-center gap-[32px] relative top-[60px] left-[31px]">
+                          <div className="inline-flex flex-col items-center gap-[15px] relative flex-[0_0_auto]">
+                            <img className="relative w-[158.68px] h-[143.48px]" alt="Group" src="group-2248.svg" />
+                            <div className="relative w-[140.85px] h-[13.01px] bg-[#cbd4e138] rounded-[70.42px/6.51px] blur-[4.1px]" />
+                          </div>
+                          <p className="relative w-[247px] ml-[-1.00px] mr-[-1.00px] [font-family:'Montserrat-Medium',Helvetica] font-normal text-transparent text-[24px] text-center tracking-[0.96px] leading-[31.2px]">
+                            <span className="font-medium text-[#666666]">
+                              Best in <br />
+                            </span>
+                            <span className="[font-family:'Montserrat-Bold',Helvetica] font-bold text-[#0072c5]">
+                              Property Listing
+                            </span>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="relative w-[287px] h-[438px]">
+                      <div className="relative w-[307px] h-[458px] top-[-6px] left-[-10px]  bg-[100%_100%]">
+                        <div className="flex flex-col w-[232px] items-center gap-[32px] relative top-[84px] left-[38px]">
+                          <div className="relative w-[192px] h-[188px]">
+                            <div className="inline-flex flex-col items-center gap-[18px] relative">
+                              <img className="relative w-[191.24px] h-[157px]" alt="Group" src="group-2247.svg" />
+                              <div className="relative w-[192px] h-[13px] bg-[#cbd4e138] rounded-[96px/6.5px] blur-[4.1px]" />
+                            </div>
+                          </div>
+                          <p className="w-[232px] relative [font-family:'Montserrat-Medium',Helvetica] font-normal text-transparent text-[24px] text-center tracking-[0.96px] leading-[31.2px]">
+                            <span className="font-medium text-[#666666]">
+                              Best in <br />
+                            </span>
+                            <span className="[font-family:'Montserrat-Bold',Helvetica] font-bold text-[#0072c5]">
+                              Trusted Services
+                            </span>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="relative w-[287px] h-[374px]">
+                      <div className="relative w-[307px] h-[394px] top-[-6px] left-[-10px]  bg-[100%_100%]">
+                        <div className="inline-flex flex-col items-center gap-[32px] relative top-[60px] left-[46px]">
+                          <div className="inline-flex flex-col items-center justify-center gap-[14px] relative flex-[0_0_auto]">
+                            <img className="relative w-[158.68px] h-[143.48px]" alt="Group" src="group-2246.svg" />
+                            <div className="relative w-[140.85px] h-[13.01px] bg-[#cbd4e138] rounded-[70.42px/6.51px] blur-[4.1px]" />
+                          </div>
+                          <p className="w-[209px] relative [font-family:'Montserrat-Medium',Helvetica] font-normal text-transparent text-[24px] text-center tracking-[0.96px] leading-[31.2px]">
+                            <span className="font-medium text-[#666666]">
+                              Best in <br />
+                            </span>
+                            <span className="[font-family:'Montserrat-Bold',Helvetica] font-bold text-[#0072c5]">
+                              Project Listing
+                            </span>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="absolute w-[279px] h-[25px] top-[471px] left-[341px] bg-[#ececec] rounded-[139.5px/12.5px] blur-[19px]" />
+                  <div className="absolute w-[279px] h-[25px] top-[438px] left-0 bg-[#ececec] rounded-[139.5px/12.5px] blur-[19px]" />
+                  <div className="absolute w-[279px] h-[25px] top-[438px] left-[678px] bg-[#ececec] rounded-[139.5px/12.5px] blur-[19px]" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+  );
+};
+
+export default WhyCHoose;
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import HomeSearch from "./components/molecules/home-search"
+import WhyCHoose from "./components/molecules/whychoose"
+
 import style from "./styles/home.module.css"
 import { signIn, signOut, useSession } from "next-auth/react"
 
@@ -7,6 +9,7 @@ export default function Home() {
   return (
  <div className="h-screen">
   <HomeSearch/>
+  <WhyCHoose />
  </div>
   )
 }
