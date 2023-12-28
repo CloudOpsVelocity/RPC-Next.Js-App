2aaa9eb5d7d3575100dce82093c9911fac775b8b
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2023-12-28 17:58:01 +0530

    Loading Added

--- app/components/atoms/Logo.tsx
+++ app/components/atoms/Logo.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Logo = () => {
   return (
-    <div className="flex justify-center items-center w-full">
+    <Link href={"/"} className="flex justify-center items-center w-full">
       <Image src={"/logo.png"} alt="logo" width={220} height={80} />
-    </div>
+    </Link>
   );
 };
 
