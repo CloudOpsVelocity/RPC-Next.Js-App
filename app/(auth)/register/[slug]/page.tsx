4bbfe8d29baef533985e0abaa665dd294a4f097b
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2023-12-28 16:40:42 +0530

    Registration Flow COmpletd

--- app/(auth)/register/[slug]/page.tsx
+++ app/(auth)/register/[slug]/page.tsx
@@ -1,17 +1,28 @@
+import Agent from "@/app/components/molecules/auth/signup/agent";
+import Individual from "@/app/components/molecules/auth/signup/individual";
+import { notFound } from "next/navigation";
 import React from "react";
 interface Props {
   params: {
     slug: string;
   };
 }
-const T = {
-  individual: "Individual",
-  company: "Company",
+
+type PageType = {
+  individual: React.ReactElement;
+  agent: React.ReactElement;
 };
 
-const Page: React.FC<Props> = async ({ params: { slug } }) => {
-  const component = T[slug];
-  return <div>{JSON.stringify(component)}</div>;
+const Page: React.FC<{ params: { slug: keyof PageType } }> = ({
+  params: { slug },
+}) => {
+  const components: PageType = {
+    individual: <Individual />,
+    agent: <Agent />,
+  };
+  const ComponentToRender = components[slug] || notFound();
+
+  return <div>{ComponentToRender}</div>;
 };
 
 export default Page;
--- app/data/userTypeDetails.ts
+++ app/data/userTypeDetails.ts
@@ -2,13 +2,16 @@ export const data = [
   {
     src: "/home/homeSeachProperty.svg",
     name: "As Individual",
+    href: "/register/individual",
   },
   {
     src: "/home/homeSeachProperty.svg",
     name: "Agent",
+    href: "/register/agent",
   },
   {
     src: "/home/homeSeachProperty.svg",
     name: "Builder",
+    href: "/register/builder",
   },
 ];
