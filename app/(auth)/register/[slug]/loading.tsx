cf2a6c73fc695f59d74420b8eeaafe3ccc35f212
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2023-12-28 16:56:04 +0530

    Update loading.tsx

--- app/(auth)/register/[slug]/loading.tsx
+++ app/(auth)/register/[slug]/loading.tsx
@@ -1,5 +1,10 @@
+import { Loader } from "@mantine/core";
 import React from "react";
 
 export default function loading() {
-  return <div>loading</div>;
+  return (
+    <div className="flex justify-center items-center h-60">
+      <Loader />
+    </div>
+  );
 }
