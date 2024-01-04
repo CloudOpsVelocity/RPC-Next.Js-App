20148bf441b7a3bcdbc4e1bd4d83dcc298bad7c1
rpc-vinay <131428813+rpc-vinay@users.noreply.github.com>
2024-01-04 13:11:25 +0530

    working with property details page in project.

--- app/data/projectDetails.ts
+++ app/data/projectDetails.ts
@@ -13,3 +13,19 @@ export const topics = [
   "FAQ?",
   "Similar",
 ];
+
+export const propertyDetailsTypes = new Map([
+  [35, { id: 35, name: "Apartment", url: "" }],
+  [33, { id: 33, name: "Row House", url: "" }],
+  [31, { id: 31, name: "Villa", url: "" }],
+  [34, { id: 34, name: "Villament", url: "" }],
+  [32, { id: 32, name: "Plot", url: "" }],
+]);
+
+export const projectprops = {
+  villa: 31,
+  plot: 32,
+  rowHouse: 33,
+  villament: 34,
+  apartment: 35,
+};
