6ad4345acd59f59b09dcd5f7f5b493a3928884f1
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2024-01-05 16:37:06 +0530

    Env Setup

--- app/components/project/spec.tsx
+++ app/components/project/spec.tsx
@@ -4,128 +4,125 @@ import { MdOutlineDoorSliding } from "react-icons/md";
 
 export default function Spec() {
   return (
-    <>
-      <div className="w-[90%] mx-auto mb-[5%] max-h-[539px]">
-        <div className="bg-white  rounded-lg shadow-md flex  overflow-hidden max-h-[539px] ">
-          <div className="flex-1 bg-gradient-to-tr from-blue-100 p-8">
-            <h2 className="text-2xl font-semibold">
-              SPECIFICATION OF
-              <span className="!text-green-600"> SARANG</span>
-            </h2>
-            <p className="text-lg text-gray-600 mt-2 mb-4 flex items-start">
-              <QuotesIcon />
-              Vital Details: Size, Amenities,
-              <br /> Features- Unveiling your dream project
-              <QuotesIcon />
-            </p>
-            <div className="flex flex-wrap gap-4">
-              <a
-                href="#structure"
-                className="border px-5 py-2 rounded-lg bg-white font-semibold flex gap-2 items-center"
-              >
-                <MdOutlineDoorSliding size={20} /> Structure
-              </a>
+    <div className="w-[90%] mx-auto mb-[5%] max-h-[539px]">
+      <div className="bg-white  rounded-lg shadow-md flex  overflow-hidden max-h-[539px] ">
+        <div className="flex-1 bg-gradient-to-tr from-blue-100 p-8">
+          <h2 className="text-2xl font-semibold">
+            SPECIFICATION OF
+            <span className="!text-green-600"> SARANG</span>
+          </h2>
+          <p className="text-lg text-gray-600 mt-2 mb-4 flex items-start">
+            <QuotesIcon />
+            Vital Details: Size, Amenities,
+            <br /> Features- Unveiling your dream project
+            <QuotesIcon />
+          </p>
+          <div className="flex flex-wrap gap-4">
+            <a
+              href="#structure"
+              className="border px-5 py-2 rounded-lg bg-white font-semibold flex gap-2 items-center"
+            >
+              <MdOutlineDoorSliding size={20} /> Structure
+            </a>
 
-              <a
-                href="#doors"
-                className="border px-5 py-2 rounded-lg bg-white font-semibold flex gap-2 items-center"
-              >
-                <MdOutlineDoorSliding size={20} /> Flooring
-              </a>
+            <a
+              href="#doors"
+              className="border px-5 py-2 rounded-lg bg-white font-semibold flex gap-2 items-center"
+            >
+              <MdOutlineDoorSliding size={20} /> Flooring
+            </a>
 
-              <a
-                href="#structure"
-                className="border px-5 py-2 rounded-lg bg-white font-semibold flex gap-2 items-center"
-              >
-                <MdOutlineDoorSliding size={20} /> Doors
-              </a>
+            <a
+              href="#structure"
+              className="border px-5 py-2 rounded-lg bg-white font-semibold flex gap-2 items-center"
+            >
+              <MdOutlineDoorSliding size={20} /> Doors
+            </a>
 
-              <a
-                href="#structure"
-                className="border px-5 py-2 rounded-lg bg-white font-semibold flex gap-2 items-center"
-              >
-                <MdOutlineDoorSliding size={20} /> Power Backup
-              </a>
-              <a
-                href="#structure"
-                className="border px-5 py-2 rounded-lg bg-white font-semibold flex gap-2 items-center"
-              >
-                <MdOutlineDoorSliding size={20} /> Windows
-              </a>
+            <a
+              href="#structure"
+              className="border px-5 py-2 rounded-lg bg-white font-semibold flex gap-2 items-center"
+            >
+              <MdOutlineDoorSliding size={20} /> Power Backup
+            </a>
+            <a
+              href="#structure"
+              className="border px-5 py-2 rounded-lg bg-white font-semibold flex gap-2 items-center"
+            >
+              <MdOutlineDoorSliding size={20} /> Windows
+            </a>
+          </div>
+        </div>
+        <div className="flex-1 bg-gray-50 p-4 rounded-lg overflow-y-scroll">
+          <div id="structure">
+            <h1 className="bg-gradient-to-tr from-blue-100 flex items-center gap-2 text-xl py-2 px-2 rounded-xl">
+              <MdOutlineDoorSliding size={22} />{" "}
+              <span className="">Structure</span>
+            </h1>
+            <div>
+              <ul className=" list-disc ml-8 grid gap-2 my-2">
+                <li>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </li>
+              </ul>
             </div>
           </div>
-          <div className="flex-1 bg-gray-50 p-4 rounded-lg overflow-y-scroll">
-            <div id="structure">
-              <h1 className="bg-gradient-to-tr from-blue-100 flex items-center gap-2 text-xl py-2 px-2 rounded-xl">
-                <MdOutlineDoorSliding size={22} />{" "}
-                <span className="">Structure</span>
-              </h1>
-              <div>
-                <ul className=" list-disc ml-8 grid gap-2 my-2">
-                  <li>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  </li>
-                  <li>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  </li>
-                  <li>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  </li>
-                  <li>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  </li>
-                </ul>
-              </div>
-            </div>
 
-            <div id="flooring">
-              <h1 className="bg-gradient-to-tr from-blue-100 flex items-center gap-2 text-xl py-2 px-2 rounded-xl">
-                <MdOutlineDoorSliding size={22} />{" "}
-                <span className="">Flooring</span>
-              </h1>
-              <div>
-                <ul className=" list-disc ml-8 grid gap-2 my-2">
-                  <li>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  </li>
-                  <li>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  </li>
-                  <li>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  </li>
-                  <li>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  </li>
-                </ul>
-              </div>
+          <div id="flooring">
+            <h1 className="bg-gradient-to-tr from-blue-100 flex items-center gap-2 text-xl py-2 px-2 rounded-xl">
+              <MdOutlineDoorSliding size={22} />{" "}
+              <span className="">Flooring</span>
+            </h1>
+            <div>
+              <ul className=" list-disc ml-8 grid gap-2 my-2">
+                <li>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </li>
+              </ul>
             </div>
+          </div>
 
-            <div id="doors">
-              <h1 className="bg-gradient-to-tr from-blue-100 flex items-center gap-2 text-xl py-2 px-2 rounded-xl">
-                <MdOutlineDoorSliding size={22} />{" "}
-                <span className="">Doors</span>
-              </h1>
-              <div>
-                <ul className=" list-disc ml-8 grid gap-2 my-2">
-                  <li>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  </li>
-                  <li>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  </li>
-                  <li>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  </li>
-                  <li>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  </li>
-                </ul>
-              </div>
+          <div id="doors">
+            <h1 className="bg-gradient-to-tr from-blue-100 flex items-center gap-2 text-xl py-2 px-2 rounded-xl">
+              <MdOutlineDoorSliding size={22} /> <span className="">Doors</span>
+            </h1>
+            <div>
+              <ul className=" list-disc ml-8 grid gap-2 my-2">
+                <li>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </li>
+              </ul>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
--- app/options.ts
+++ app/options.ts
@@ -21,9 +21,7 @@ export const options: NextAuthOptions = {
       // @ts-ignore
       async authorize(credentials) {
         const res = await axios.post(
-          process.env.NODE_ENV === "development"
-            ? "http://localhost:8081/user/v1/doLoginWithMobile"
-            : `${process.env.BACKEND_URL!!}/user/v1/doLoginWithMobile`,
+          `${process.env.BACKEND_URL}/user/v1/doLoginWithMobile`,
           {
             username: credentials?.username,
             password: credentials?.password,
