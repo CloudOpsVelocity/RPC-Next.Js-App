b34d0877aa83ca742a69b14971459d44ed6eb0c4
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2024-01-10 19:43:03 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "",
-  assetPrefix: "https://test.getrightproperty.com",
+  // basePath: "",
+  // assetPrefix: "https://test.getrightproperty.com",
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/(.*)",
+  //       headers: [
+  //         {
+  //           key: "Strict-Transport-Security",
+  //           value: "max-age=31536000; includeSubDomains; preload",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
   // eslint: {
   //   ignoreDuringBuilds: true,
   // },
