191bffd628a7975fd1515274c859e436f1ef7fea
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2024-01-10 19:06:42 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "",
   assetPrefix: "https://test.getrightproperty.com",
   // eslint: {
   //   ignoreDuringBuilds: true,
