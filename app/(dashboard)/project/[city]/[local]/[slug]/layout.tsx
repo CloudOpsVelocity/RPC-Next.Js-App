ef1c59989069c88c4c1ff3110317b7b687656fb9
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2024-01-10 19:56:34 +0530

    Bug Fix Project Carousel

--- app/(dashboard)/project/[city]/[local]/[slug]/layout.tsx
+++ app/(dashboard)/project/[city]/[local]/[slug]/layout.tsx
@@ -1,6 +1,6 @@
 import Footer from "@/app/components/layouts/primary/footer";
 import Header from "@/app/components/layouts/primary/header";
-import ProjectDrawer from "@/app/components/project/Drawer";
+import "@mantine/carousel/styles.css";
 import React from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
