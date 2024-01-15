223f8b022716ca97c76ab6f986eaad08615d313b
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2024-01-15 19:41:06 +0530

    Update index.ts

--- app/validations/auth/index.ts
+++ app/validations/auth/index.ts
@@ -26,7 +26,7 @@ const agentSchema = yup.object().shape({
   userName: yup
     .string()
     .matches(/^[a-zA-Z\s]*$/, "Only letters and spaces are allowed")
-    .max(128, "Name should not exceed 128 characters")
+    .max(20, "Name should not exceed 20 characters")
     .required("Full name is required"),
   email: yup.string().email("Invalid email").required("Email is required"),
   password: yup
