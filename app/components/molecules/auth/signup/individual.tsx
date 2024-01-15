f9b0d006241447721375f8b6d2b9c672ecc41a38
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2024-01-15 18:50:52 +0530

    Individiual

--- app/components/molecules/auth/signup/individual.tsx
+++ app/components/molecules/auth/signup/individual.tsx
@@ -61,118 +61,122 @@ function Individual() {
   };
 
   return (
-    <Box className="w-full max-w-[423px] mt-[3%] " mx="auto">
-      <div className="w-full max-w-[459px] md:max-w-[597px] flex justify-center items-center gap-[5%] mb-[5%] ">
-        <Link
-          href="/login"
-          className="whitespace-nowrap text-[26px] font-[500] text-[#666]"
-        >
-          Log In
-        </Link>
-
-        <Link
-          href="/register"
-          className="whitespace-nowrap text-[26px] text-[#148B16] font-bold border-solid border-b-2 border-green-600"
-        >
-          Individual Sign Up
-        </Link>
-      </div>
+    <>
       {status === "success" ? (
         <Success />
       ) : (
-        <form
-          onSubmit={form.onSubmit(onSubmit)}
-          className="w-[100%] flex justify-center items-center flex-col"
-        >
-          <TextInput
-            required
-            size="md"
-            className="w-[100%] mb-[3%] "
-            label="Full Name"
-            placeholder="Enter your name here"
-            {...form.getInputProps("name")}
-          />
-          <TextInput
-            type="email"
-            required
-            mt="sm"
-            size="md"
-            className="w-[100%] mb-[3%] "
-            label="Email"
-            placeholder="Enter your email here"
-            {...form.getInputProps("email")}
-          />
-          <PasswordInput
-            classNames={{
-              visibilityToggle: P.visibilityToggle,
-            }}
-            required
-            size="md"
-            mt="sm"
-            className="w-[100%] mb-[3%] "
-            label="Password"
-            placeholder="Enter your password here"
-            {...form.getInputProps("password")}
-          />
-          <NumberInput
-            required
-            classNames={{
-              input: S.input,
-            }}
-            hideControls
-            size="md"
-            mt="sm"
-            className="w-[100%] mb-[3%] "
-            label="Contact Number"
-            placeholder="Enter your contact number here"
-            {...form.getInputProps("mobile")}
-            maxLength={10}
-          />
+        <>
+          <Box className="w-full max-w-[423px] mt-[3%] " mx="auto">
+            <div className="w-full max-w-[459px] md:max-w-[597px] flex justify-center items-center gap-[5%] mb-[5%] ">
+              <Link
+                href="/login"
+                className="whitespace-nowrap text-[26px] font-[500] text-[#666]"
+              >
+                Log In
+              </Link>
 
-          <CountryInput
-            onSelect={displayCountryCode}
-            className={`focus:outline-none min-w-[30px] max-w-[70px] self-start relative ${
-              form.errors.mobile != undefined && form.errors.mobile != null
-                ? "bottom-[65px]"
-                : "bottom-[45px]"
-            }  ml-[2px]`}
-          />
-
-          <div className="w-full flex justify-between items-center flex-wrap">
-            <Button
-              mt="sm"
-              onClick={() => router.back()}
-              className="!rounded-[6px] !border-solid !border-1 !border-blue-600 !bg-[#FFF] !text-[#0073C6] !w-[100%] !max-w-[178px]  "
+              <Link
+                href="/register"
+                className="whitespace-nowrap text-[26px] text-[#148B16] font-bold border-solid border-b-2 border-green-600"
+              >
+                Individual Sign Up
+              </Link>
+            </div>
+            <form
+              onSubmit={form.onSubmit(onSubmit)}
+              className="w-[100%] flex justify-center items-center flex-col"
             >
-              Back
-            </Button>
-            <Button
-              loading={status === "pending"}
-              type="submit"
-              mt="sm"
-              className="!rounded-[6px] !w-[100%] !max-w-[225px] !bg-[#0c7aca]"
-            >
-              SAVE & VERIFY
-            </Button>
-          </div>
+              <TextInput
+                required
+                size="md"
+                className="w-[100%] mb-[3%] "
+                label="Full Name"
+                placeholder="Enter your name here"
+                {...form.getInputProps("name")}
+              />
+              <TextInput
+                type="email"
+                required
+                mt="sm"
+                size="md"
+                className="w-[100%] mb-[3%] "
+                label="Email"
+                placeholder="Enter your email here"
+                {...form.getInputProps("email")}
+              />
+              <PasswordInput
+                classNames={{
+                  visibilityToggle: P.visibilityToggle,
+                }}
+                required
+                size="md"
+                mt="sm"
+                className="w-[100%] mb-[3%] "
+                label="Password"
+                placeholder="Enter your password here"
+                {...form.getInputProps("password")}
+              />
+              <NumberInput
+                required
+                classNames={{
+                  input: S.input,
+                }}
+                hideControls
+                size="md"
+                mt="sm"
+                className="w-[100%] mb-[3%] "
+                label="Contact Number"
+                placeholder="Enter your contact number here"
+                {...form.getInputProps("mobile")}
+                maxLength={10}
+              />
 
-          <p className="text-[20px] font-[400] text-[#202020] mt-[5%]">
-            Already have an Account ?{" "}
-            <Link
-              href="/login"
-              className="text-[20px] font-[600] text-[#0073C6]"
-            >
-              Log In
-            </Link>
-          </p>
+              <CountryInput
+                onSelect={displayCountryCode}
+                className={`focus:outline-none min-w-[30px] max-w-[70px] self-start relative ${
+                  form.errors.mobile != undefined && form.errors.mobile != null
+                    ? "bottom-[65px]"
+                    : "bottom-[45px]"
+                }  ml-[2px]`}
+              />
 
-          <Link
-            href="/"
-            className="text-[20px] font-[700] text-[#148B16] underline "
-          >
-            Continue Without Register
-          </Link>
-        </form>
+              <div className="w-full flex justify-between items-center flex-wrap">
+                <Button
+                  mt="sm"
+                  onClick={() => router.back()}
+                  className="!rounded-[6px] !border-solid !border-1 !border-blue-600 !bg-[#FFF] !text-[#0073C6] !w-[100%] !max-w-[178px]  "
+                >
+                  Back
+                </Button>
+                <Button
+                  loading={status === "pending"}
+                  type="submit"
+                  mt="sm"
+                  className="!rounded-[6px] !w-[100%] !max-w-[225px] !bg-[#0c7aca]"
+                >
+                  SAVE & VERIFY
+                </Button>
+              </div>
+
+              <p className="text-[20px] font-[400] text-[#202020] mt-[5%]">
+                Already have an Account ?{" "}
+                <Link
+                  href="/login"
+                  className="text-[20px] font-[600] text-[#0073C6]"
+                >
+                  Log In
+                </Link>
+              </p>
+
+              <Link
+                href="/"
+                className="text-[20px] font-[700] text-[#148B16] underline "
+              >
+                Continue Without Register
+              </Link>
+            </form>
+          </Box>
+        </>
       )}
 
       <AuthPopup
@@ -183,7 +187,7 @@ function Individual() {
         userName={form.values.email}
         mobile={form.values.mobile && form.values.mobile}
       />
-    </Box>
+    </>
   );
 }
 
--- app/components/molecules/auth/success.tsx
+++ app/components/molecules/auth/success.tsx
@@ -1,16 +1,19 @@
+"use client";
+import { useParams } from "next/navigation";
 import React from "react";
 
-export default function Success() {
+export default function Success({ params }: any) {
+  const P = useParams();
   return (
-    <div className="flex  w-full flex-col items-center justify-center  p-4">
-      <div className="w-full max-w-xs rounded-lg bg-white p-8 text-center ">
-        <h1 className="mt-4 text-2xl font-bold text-green-500">
+    <div className="flex  max-w-3xl flex-col items-center justify-center  p-4 ">
+      <div className="w-full  rounded-lg bg-white p-8 text-center ">
+        <h1 className="text-[color:var(--Brand-green-primary,#148B16)] text-[28px] not-italic font-bold leading-[normal]">
           Congratulations!
         </h1>
-        <p className="mt-2 text-sm text-gray-600">
-          Your account for agent has been created successfully
+        <p className="text-[color:var(--Grey-1,#666)] text-center text-[26px] not-italic font-medium leading-[normal] mt-10">
+          Your account for {P.slug} has been created successfully
         </p>
-        <p className="mt-8 text-xs text-gray-500">
+        <p className="text-[color:var(--Grey-2,#767270)] text-center text-[26px] not-italic font-medium leading-[normal] mt-64">
           You will be redirected to homepage in 5 sec
         </p>
       </div>
