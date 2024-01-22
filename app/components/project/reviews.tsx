0953d8d51184f2ee4cb775470df12284a30f7dfa
rpc-vinay <131428813+rpc-vinay@users.noreply.github.com>
2024-01-22 13:03:05 +0530

    Update reviews.tsx

--- app/components/project/reviews.tsx
+++ app/components/project/reviews.tsx
@@ -4,10 +4,15 @@ import { Carousel } from "@mantine/carousel";
 import React from "react";
 import { ReviewBoxIcon, quotesIcon } from "@/app/images/commonSvgs";
 import { Rating } from "@mantine/core";
+import useRatings from "@/app/hooks/useRatings";
+import { useParams } from "next/navigation";
 
 export default function Reviews() {
+  const {slug} = useParams<{slug:string}>()
+  const {data,isLoading} = useRatings({projectId:slug})
   return (
     <div className="bg-[#FFF] py-12 w-full ">
+      {JSON.stringify(data)}
       <div className="max-w-[1920px] mx-auto px-6">
         <h2 className="text-2xl font-semibold text-gray-800">
           CUSTOMER REVIEWS FOR <span className="text-green-500">SARANG</span>
@@ -53,21 +58,22 @@ export default function Reviews() {
 const Review = () => {
   return (
     <>
-      <div className="max-w-lg mx-auto bg-white rounded-lg p-4 shadow-md border-solid border-[#DCE6ED] border-[1px] ">
+      <div className="max-w-lg mx-auto mt-[20px] bg-white rounded-lg p-4 relative shadow-md border-solid border-[#DCE6ED] border-[1px] ">
+      <span className=" absolute top-[-20px] !z-30  ">{quotesIcon}</span>
         <div className="flex items-center space-x-2">
           <div className="flex-1">
+
             <div className="flex justify-between items-center">
               <div>
-                <span className=" relative mb-[-40px] bottom-[30px] !z-10 ">{quotesIcon}</span>
-                <p className="text-lg font-semibold">Ankita Soni</p>
-                <p className="text-sm text-gray-500">Owner</p>
+                <p className="text-[18px] text-[#000] font-[500] mt-[20px]">Ankita Soni</p>
+                <p className="text-[14px] text-[#212C33] font-[500]">Owner</p>
               </div>
               <div className="text-right">
                 <Rating size={"sm"} value={5} fractions={2} readOnly />
                 <span className="text-xs text-gray-500">3 days ago</span>
               </div>
             </div>
-            <p className="mt-2 text-gray-600 text-sm">
+            <p className="mt-2 text-[14px] text-[#3E3E3E] font-[400]">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do ei
               usmod tempor incididunt ut labore et dolore magna aliqua. Uten im
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa,
