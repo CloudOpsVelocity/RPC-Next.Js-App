275ab6543c5c451b4b5d4dee9bd2ae5585807877
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2024-01-23 16:16:44 +0530

    some changes

--- app/components/project/downloadBroucher.tsx
+++ app/components/project/downloadBroucher.tsx
@@ -1,17 +1,23 @@
 "use client";
+import { downLoadIcon } from "@/app/images/commonSvgs";
 import React from "react";
 
-// import { downloadIcon } from '@/app/images/commonSvgs';
-
 const DownloadBroucher = ({ url }: { url: string }) => {
-  const onButtonClick = () => {
-    const pdfUrl = "Sample.pdf";
-    const link = document.createElement("a");
-    link.href = pdfUrl;
-    link.download = url; // specify the filename
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+  const onButtonClick = async () => {
+    try {
+      const response = await fetch(url);
+      const blob = await response.blob();
+      const blobUrl = window.URL.createObjectURL(blob);
+
+      const link = document.createElement("a");
+      link.href = blobUrl;
+      link.download = "Brochure.pdf"; // specify the filename
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } catch (error) {
+      console.error("Error downloading the file:", error);
+    }
   };
 
   return (
@@ -23,7 +29,7 @@ const DownloadBroucher = ({ url }: { url: string }) => {
         onClick={() => onButtonClick()}
         className="flex border-0 justify-center items-center text-[20px] lg:text-[24px] text-[#FFF] font-[700] rounded-[10px] bg-[#0073C6] gap-[8px] p-[5px]  "
       >
-        {/* {downloadIcon} */}
+        {downLoadIcon}
         Download Brochure
       </button>
     </div>
