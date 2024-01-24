c1113259caa339ec1b649d21f09e9dd16d4b1f65
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2024-01-24 18:08:25 +0530

    Update date.ts

--- app/utils/date.ts
+++ app/utils/date.ts
@@ -2,6 +2,7 @@ function formatDate(inputDate: string | undefined): string {
   if (inputDate == null) {
     return ""; // You can return an empty string or any default value
   }
+
   // Convert the input string to a Date object
   const date = new Date(inputDate.replace(/IST/, "GMT+0530"));
 
@@ -10,23 +11,14 @@ function formatDate(inputDate: string | undefined): string {
     return "Invalid Date";
   }
 
-  // Get the day of the week (0-6, where 0 is Sunday and 6 is Saturday)
-  const dayOfWeek = date.getDay();
-
-  // Calculate the number of milliseconds in one day
-  const oneDay = 24 * 60 * 60 * 1000;
-
-  // Calculate the new date by subtracting one day
-  const newDate = new Date(date?.getTime() - oneDay);
-
-  // Format the new date string
-  const formattedDate = newDate.toLocaleDateString("en-US", {
-    weekday: "short",
-    month: "short",
+  // Format the date string with the desired format
+  const formattedDate = date.toLocaleDateString("en-US", {
     day: "numeric",
+    month: "long",
     year: "numeric",
   });
 
   return formattedDate;
 }
+
 export { formatDate };
