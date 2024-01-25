a1f687888222ce3695b2dd2092a8d762e2b980b0
rpc-vinay <131428813+rpc-vinay@users.noreply.github.com>
2024-01-25 15:55:39 +0530

    changes

--- app/utils/api/actions/shortlist.ts
+++ app/utils/api/actions/shortlist.ts
@@ -9,7 +9,7 @@ export const addShortList = async (data: Props) => {
   const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/user-actions/add?type=${data.type}`;
   try {
     const response = await axios.post(url, { projIdEnc: data.projIdEnc, isactive: data.isactive });
-    toast.success("Property Shortlisted");
+    toast.success(data.isactive == "Y" ? "Property Added" : "Property Removed");
     return response.data;
   } catch (error) {
     toast.error("Something went wrong");
