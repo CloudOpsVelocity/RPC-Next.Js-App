ba74714391e236d532538424d39e25808fd978a9
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2024-01-29 12:43:21 +0530

    Update Carousel.tsx

--- app/components/project/modals/Carousel.tsx
+++ app/components/project/modals/Carousel.tsx
@@ -2,11 +2,11 @@ import { Dispatch, SetStateAction, useState } from "react";
 import { Button, Modal, rem } from "@mantine/core";
 import { Carousel } from "@mantine/carousel";
 import { PopupOpenSvg, emptyFilesIcon } from "@/app/images/commonSvgs";
-import { MiddleSection, RightSection } from "./FloorPlan";
+import { RightSection } from "./FloorPlan";
 import S from "@/app/styles/ModalCarousel.module.css";
-import { useAtomValue } from "jotai";
+import { useAtom, useAtomValue } from "jotai";
 import { floorPlansArray, selectedFloorAtom } from "@/app/store/floor";
-import Image from "next/image";
+import { Image } from "@mantine/core";
 
 type CarouselModalProps = {
   opened: boolean;
@@ -30,8 +30,8 @@ function CarouselModal({ opened, setOpened }: CarouselModalProps) {
           close: S.close,
         }}
       >
-        <div className="flex flex-col md:flex-row p-[2%]  mb-10 justify-center items-start gap-[45px] shrink-0">
-          <MiddleSection hide={true} />
+        <div className="flex flex-col md:flex-row p-[2%]  mb-10 justify-center items-center gap-[45px] shrink-0">
+          <MiddleSection />
           <RightSection propCgId={""} />
         </div>
       </Modal>
@@ -40,3 +40,30 @@ function CarouselModal({ opened, setOpened }: CarouselModalProps) {
 }
 
 export default CarouselModal;
+
+const MiddleSection = () => {
+  const data = useAtomValue(selectedFloorAtom);
+
+  return (
+    <div className="max-w-[1400px]">
+      <p className="text-[#005DA0] w-full  mb-[1%] text-[16px] font-[500]  text-left">
+        Sarang by sumadhura/2bhk/tower 1/ 04/north/1124 sq.ft
+      </p>
+      {data?.floorPlanUrl ? (
+        <Image
+          // @ts-ignore
+          src={data.floorPlanUrl}
+          radius="md"
+          h={750}
+          w={1500}
+          fit="cover"
+        />
+      ) : (
+        <div>
+          {emptyFilesIcon}
+          <p>No Matching Results Found !</p>
+        </div>
+      )}
+    </div>
+  );
+};
