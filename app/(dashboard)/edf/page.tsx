1ec63366a7e4f84754df7b3f672e348198ddb3a8
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2024-02-22 18:15:12 +0530

    ++++ img test

--- app/(dashboard)/edf/page.tsx
+++ app/(dashboard)/edf/page.tsx
@@ -5,7 +5,13 @@ type Props = { searchParams: { path: string } };
 export default function Page({ searchParams: { path } }: Props) {
   return (
     <div className="flex justify-center items-center h-[100vh]">
-      <Image radius="md" src={`${path}`} h={600} w="auto" fit="contain" />
+      <Image
+        radius="md"
+        src={`${process.env.NEXT_PUBLIC_IMG_BASE}${path}`}
+        h={600}
+        w="auto"
+        fit="contain"
+      />
     </div>
   );
 }
