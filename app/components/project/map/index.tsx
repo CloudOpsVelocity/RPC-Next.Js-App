516b33acbc148ec3c9ae79c7407b234458817e2a
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2024-03-14 18:11:32 +0530

    icon updated

--- app/components/project/map/index.tsx
+++ app/components/project/map/index.tsx
@@ -88,7 +88,7 @@ const LeafMap: React.FC<{
           <Button
             buttonClass="flex justify-center items-center gap-1 p-2.5 border rounded-[21px] border-solid border-[#0094FF] text-[#202020] text-xl not-italic font-semibold leading-[normal] bg-[#F2FAFF] mt-3 ml-auto "
             title={"Localte Project"}
-            icon={<GradientLocation />}
+            icon={<GradientLocation className="w-8 h-8" />}
           />
         </div>
       </div>
