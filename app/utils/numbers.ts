1fb1ecd9fc898943264653012955d11151c03241
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2024-05-01 16:04:05 +0530

    Update numbers.ts

--- app/utils/numbers.ts
+++ app/utils/numbers.ts
@@ -2,11 +2,17 @@ function formatCurrency(value: number) {
   if (value >= 10000000) {
     // If value is 1 crore or more, format in crores
     const croreValue = (value / 10000000).toFixed(2);
-    return `₹ ${croreValue} Cr`;
+    const formattedValue = croreValue.endsWith(".00")
+      ? croreValue.slice(0, -3)
+      : croreValue;
+    return `₹ ${formattedValue} Cr`;
   } else if (value >= 100000) {
     // If value is 1 lakh or more, format in lakhs
     const lakhValue = (value / 100000).toFixed(2);
-    return `₹ ${lakhValue} Lac`;
+    const formattedValue = lakhValue.endsWith(".00")
+      ? lakhValue.slice(0, -3)
+      : lakhValue;
+    return `₹ ${formattedValue} Lac`;
   } else {
     // If value is less than 1 lakh, format as is
     return `₹ ${value}`;
