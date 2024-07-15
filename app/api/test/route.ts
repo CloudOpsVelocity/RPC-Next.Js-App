168dc6529533b1ca33d89423267a2c5b610fa59d
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2024-07-15 19:36:55 +0530

    Update route.ts

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -2,10 +2,10 @@ import fs from "fs";
 import { NextResponse } from "next/server";
 export async function GET(req: Request) {
   try {
-    const data = fs.appendFileSync("test.txt", `${new Date()}\n`, {
-      encoding: "utf-8",
-    });
-    return NextResponse.json({ data });
+    // const data = fs.appendFileSync("test.txt", `${new Date()}\n`, {
+    //   encoding: "utf-8",
+    // });
+    return NextResponse.json({ data: true });
   } catch (error) {
     console.error(error);
     return Response.json({ ok: false, error: "Error reading file" });
