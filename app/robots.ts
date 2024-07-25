ed51b2273c5714bbe0451aab8160c25228c43bc4
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2024-07-25 13:45:58 +0530

    LISTING ADDED

--- app/robots.ts
+++ app/robots.ts
@@ -1,15 +1,12 @@
 import type { MetadataRoute } from "next";
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
-        userAgent: "Googlebot",
-        allow: ["/"],
-        disallow: "/test/",
-      },
-      {
-        userAgent: ["Applebot", "Bingbot"],
-        disallow: ["/test/"],
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/test/", "/dashboard/"],
       },
     ],
     sitemap: `${process.env.NEXT_PUBLIC_PROJECT_URL}/sitemap.xml`,
