5b45095e6de80ba0c0353315c027680c33868fe1
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2024-07-29 18:56:49 +0530

    Update Main.tsx

--- app/components/project/sections/Main.tsx
+++ app/components/project/sections/Main.tsx
@@ -3,7 +3,6 @@ import React, { useState } from "react";
 import InFoCarousel from "./InFoCarousel";
 import CardCarousel from "./CardCarousel";
 import PartialUnitModal from "./Modal/Modal";
-import CarouselModal from "./Modal/Carousel";
 type Props = {
   partialUnitData: any;
   data: any;
