50b204d735a28d7106ec81bcff1f77e8477fb668
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2024-08-05 17:11:55 +0530

    SOME FN DONE

--- app/(dashboard)/new/components/home-search/filters/BugdetSelect.tsx
+++ app/(dashboard)/new/components/home-search/filters/BugdetSelect.tsx
@@ -1,65 +1,134 @@
 import { useState } from "react";
-import { Combobox, Input, InputBase, Radio, useCombobox } from "@mantine/core";
+import {
+  Combobox,
+  InputBase,
+  useCombobox,
+  NumberInput,
+  Group,
+  Input,
+} from "@mantine/core";
 import styles from "./Style.module.css";
-import { DropDownIcon } from "@/app/images/commonSvgs";
-import useSearchFilters from "@/app/hooks/search";
-import { formatBudgetValue } from "@/app/(dashboard)/search/components/buget";
-
+import { useAtom } from "jotai";
+import { homeSearchFiltersAtom } from "@/app/store/home";
+const MULTIPLIER = 10000000;
 const groceries = [
-  "5L - 25L",
-  "25L - 45L",
-  "45L - 65L",
-  "65L - 85L",
-  "85L - 10.5CR",
-  "10.5CR - 30.5CR",
-  "30.5CR - 50.5CR",
-  "50.5CR - 60CR+more",
+  "5L",
+  "10L",
+  "20L",
+  "30L",
+  "40L",
+  "50L",
+  "60L",
+  "70L",
+  "80L",
+  "90L",
+  "1CR",
+  "10CR",
+  "20CR",
+  "30CR",
+  "40CR",
+  "50CR",
+  "60CR",
 ];
-const map = new Map([
-  ["5L - 25L", { min: 0.05, max: 0.25 }],
-  ["25L - 45L", { min: 0.25, max: 0.45 }],
-  ["45L - 65L", { min: 0.45, max: 0.65 }],
-  ["65L - 85L", { min: 0.65, max: 0.85 }],
-  ["85L - 10.5CR", { min: 0.85, max: 10.5 }],
-  ["10.5CR - 30.5CR", { min: 10.5, max: 30.5 }],
-  ["30.5CR - 50.5CR", { min: 30.5, max: 50.5 }],
-  ["50.5CR - 60CR+more", { min: 50.5, max: 60 }],
+
+const map = new Map<string, { value: number }>([
+  ["5L", { value: 5 * MULTIPLIER }],
+  ["10L", { value: 10 * MULTIPLIER }],
+  ["20L", { value: 20 * MULTIPLIER }],
+  ["30L", { value: 30 * MULTIPLIER }],
+  ["40L", { value: 40 * MULTIPLIER }],
+  ["50L", { value: 50 * MULTIPLIER }],
+  ["60L", { value: 60 * MULTIPLIER }],
+  ["70L", { value: 70 * MULTIPLIER }],
+  ["80L", { value: 80 * MULTIPLIER }],
+  ["90L", { value: 90 * MULTIPLIER }],
+  ["1CR", { value: 100 * MULTIPLIER }],
+  ["10CR", { value: 1000 * MULTIPLIER }],
+  ["20CR", { value: 2000 * MULTIPLIER }],
+  ["30CR", { value: 3000 * MULTIPLIER }],
+  ["40CR", { value: 4000 * MULTIPLIER }],
+  ["50CR", { value: 5000 * MULTIPLIER }],
+  ["60CR", { value: 6000 * MULTIPLIER }],
 ]);
 
+const toFormattedString = (value: number) => {
+  if (value >= 100 * MULTIPLIER) {
+    return `${value / (100 * MULTIPLIER)}CR`;
+  } else {
+    return `${value / MULTIPLIER}L`;
+  }
+};
+
 export function BasicBudgetSelect() {
-  const { filters: f, handleSliderChange } = useSearchFilters();
+  const [f, dispatch] = useAtom(homeSearchFiltersAtom);
+  const [minValue, setMinValue] = useState<number>(f.bugdetValue[0]);
+  const [maxValue, setMaxValue] = useState<number>(f.bugdetValue[1]);
+  const [focusedInput, setFocusedInput] = useState<"min" | "max" | null>(null);
   const combobox = useCombobox({
     onDropdownClose: () => combobox.resetSelectedOption(),
   });
 
-  const options = groceries.map((item) => {
-    const minValue = map.get(item)?.min ?? 0;
-    const maxValue = map.get(item)?.max ?? 60;
-    const allInRange = f.bugdetValue.every(
-      (value) => value >= minValue && value <= maxValue
-    );
+  const filteredOptions = groceries.filter((item) => {
+    const value = map.get(item)?.value ?? 0;
+    if (focusedInput === "max") {
+      return value > minValue; // Show options greater than or equal to minValue
+    } else if (focusedInput === "min") {
+      return value < maxValue; // Show options less than or equal to maxValue
+    } else {
+      // Show all options if no input is focused
+      return value >= minValue && value <= maxValue;
+    }
+  });
+
+  const options = filteredOptions.map((item) => {
+    const value = map.get(item)?.value ?? 0;
+    const handleOptionSelect = () => {
+      if (focusedInput === "max") {
+        setMaxValue(value);
+        dispatch({
+          type: "SET_BUGDET_VALUE",
+          payload: [minValue, value],
+        });
+      } else {
+        setMinValue(value);
+        dispatch({
+          type: "SET_BUGDET_VALUE",
+          payload: [value, maxValue],
+        });
+      }
+    };
     return (
-      <Combobox.Option
-        value={item}
-        classNames={{
-          option: styles.option,
-        }}
-      >
-        <Radio checked={allInRange} color="green" mr={6} /> {item}
-      </Combobox.Option>
+      <div key={item} className={styles.option} onClick={handleOptionSelect}>
+        {item}
+      </div>
     );
   });
-  const shouldShowBudget = !(f.bugdetValue[0] === 0 && f.bugdetValue[1] === 60);
+
+  const handleMinChange = (val: number) => {
+    setMinValue(val);
+    dispatch({ type: "SET_BUGDET_VALUE", payload: [val, maxValue] });
+  };
+
+  const handleMaxChange = (val: number) => {
+    setMaxValue(val);
+    dispatch({ type: "SET_BUGDET_VALUE", payload: [minValue, val] });
+  };
+
+  const shouldShowBudget = !(
+    f.bugdetValue[0] === 0 && f.bugdetValue[1] === 60 * MULTIPLIER
+  );
 
   return (
     <Combobox
       store={combobox}
       withinPortal={false}
       onOptionSubmit={(val) => {
-        handleSliderChange("bugdetValue", [
-          map.get(val)!.min,
-          map.get(val)!.max,
-        ] as any);
+        const value = map.get(val)?.value ?? 0;
+        if (focusedInput === "max") {
+          handleMaxChange(value);
+        } else {
+          handleMinChange(value);
+        }
         combobox.closeDropdown();
       }}
       classNames={{
@@ -71,7 +140,6 @@ export function BasicBudgetSelect() {
         <InputBase
           component="button"
           type="button"
-          pointer
           rightSection={<DropIcon />}
           onClick={() => combobox.toggleDropdown()}
           rightSectionPointerEvents="none"
@@ -79,10 +147,9 @@ export function BasicBudgetSelect() {
             input: styles.input,
           }}
         >
-          {(shouldShowBudget &&
-            `₹${formatBudgetValue(f.bugdetValue[0])}  - ₹${formatBudgetValue(
-              f.bugdetValue[1]
-            )}`) || (
+          {shouldShowBudget ? (
+            `${toFormattedString(minValue)} - ${toFormattedString(maxValue)}`
+          ) : (
             <Input.Placeholder className="!text-black">
               Budget
             </Input.Placeholder>
@@ -91,24 +158,49 @@ export function BasicBudgetSelect() {
       </Combobox.Target>
 
       <Combobox.Dropdown>
-        <Combobox.Options>{options}</Combobox.Options>
+        <Group>
+          <NumberInput
+            placeholder="Min Price"
+            hideControls
+            value={minValue}
+            onChange={(val) => handleMinChange(val as number)}
+            onFocus={() => setFocusedInput("min")}
+            max={f.bugdetValue[1]}
+            clampBehavior="strict"
+            thousandSeparator=","
+            allowDecimal={false}
+            allowNegative={false}
+          />
+          <NumberInput
+            clampBehavior="strict"
+            placeholder="Max Price"
+            hideControls
+            value={maxValue}
+            onChange={(val) => handleMaxChange(val as number)}
+            onFocus={() => setFocusedInput("max")}
+            thousandSeparator=","
+            allowDecimal={false}
+            allowNegative={false}
+            min={f.bugdetValue[0]}
+          />
+        </Group>
+        {options}
       </Combobox.Dropdown>
     </Combobox>
   );
 }
-const DropIcon = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="26"
-      height="26"
-      viewBox="0 0 18 18"
-      fill="none"
-    >
-      <path
-        d="M12.4134 6C13.3274 6 13.7624 7.1251 13.0861 7.73994L10.1727 10.3885C9.79125 10.7352 9.20875 10.7352 8.82733 10.3885L5.91394 7.73994C5.23761 7.1251 5.67257 6 6.58661 6H12.4134Z"
-        fill="#8EA8CF"
-      />
-    </svg>
-  );
-};
+
+const DropIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="26"
+    height="26"
+    viewBox="0 0 18 18"
+    fill="none"
+  >
+    <path
+      d="M12.4134 6C13.3274 6 13.7624 7.1251 13.0861 7.73994L10.1727 10.3885C9.79125 10.7352 9.20875 10.7352 8.82733 10.3885L5.91394 7.73994C5.23761 7.1251 5.67257 6 6.58661 6H12.4134Z"
+      fill="#8EA8CF"
+    />
+  </svg>
+);
--- app/store/home/index.ts
+++ app/store/home/index.ts
@@ -1,29 +1,32 @@
 import { atomWithReducer } from "jotai/utils";
 interface SearchState {
   bhk: number | null;
-  unitType: string | null;
+  unitTypes: number[];
   locality: string[];
   city: string | null;
   cg: string;
+  bugdetValue: [number, number];
 }
 
 const initialState: SearchState = {
   bhk: null,
-  unitType: null,
+  unitTypes: [],
   locality: [],
   city: null,
   cg: "buy",
+  bugdetValue: [500000, 60000000000],
 };
 
 // Define the action types
 type Action =
   | { type: "ADD_BHK"; payload: number }
-  | { type: "ADD_UNIT_TYPE"; payload: string }
+  | { type: "ADD_UNIT_TYPE"; payload: number }
   | { type: "ADD_LOCALITY"; payload: string }
   | { type: "SET_CITY"; payload: string }
   | { type: "REMOVE_LOCALITY"; payload: string }
   | { type: "RESET_FILTERS" }
-  | { type: "SET_CG"; payload: string };
+  | { type: "SET_CG"; payload: string }
+  | { type: "SET_BUGDET_VALUE"; payload: [number, number] };
 
 // Define the reducer function
 const searchReducer = (state: SearchState, action: Action): SearchState => {
@@ -31,7 +34,7 @@ const searchReducer = (state: SearchState, action: Action): SearchState => {
     case "ADD_BHK":
       return { ...state, bhk: action.payload };
     case "ADD_UNIT_TYPE":
-      return { ...state, unitType: action.payload };
+      return { ...state, unitTypes: [...state.unitTypes, action.payload] };
     case "ADD_LOCALITY":
       return { ...state, locality: [...state.locality, action.payload] };
     case "SET_CITY":
@@ -43,6 +46,8 @@ const searchReducer = (state: SearchState, action: Action): SearchState => {
       };
     case "SET_CG":
       return { ...state, cg: action.payload };
+    case "SET_BUGDET_VALUE":
+      return { ...state, bugdetValue: action.payload };
     case "RESET_FILTERS":
       return initialState;
     default:
@@ -52,4 +57,7 @@ const searchReducer = (state: SearchState, action: Action): SearchState => {
 };
 
 // Create the atom with reducer
-export const searchFiltersAtom = atomWithReducer(initialState, searchReducer);
+export const homeSearchFiltersAtom = atomWithReducer(
+  initialState,
+  searchReducer
+);
