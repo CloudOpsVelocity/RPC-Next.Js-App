4daa00e248cb0b6b411cd9951804557209d5827f
rpc-vinay <131428813+rpc-vinay@users.noreply.github.com>
2024-08-07 18:39:34 +0530

    latest changes

--- app/(dashboard)/new/components/home-search/filters/mobile/Drawer.tsx
+++ app/(dashboard)/new/components/home-search/filters/mobile/Drawer.tsx
@@ -39,11 +39,12 @@ function AppDrawer({ close, opened }: Props) {
           <p className="inline-flex sm:hidden justify-center items-center text-[#242424] text-[14px] not-italic font-medium gap-1">
             <SmallHomeIcon /> All Residential
           </p>
-          <div className="flex justify-between items-center sm:gap-2.5 rounded shadow-[0px_4px_20px_0px_rgba(194,194,194,0.40)] px-1.5 py-1 border-[0.5px] border-solid border-[#819CA9] bg-white w-full sm:w-auto">
+          <div className="flex justify-between items-center sm:gap-2.5 rounded shadow-[0px_4px_20px_0px_rgba(194,194,194,0.40)] sm:px-1.5 py-1 border-[0.5px] border-solid border-[#819CA9] bg-white w-full sm:w-auto">
             <div className="hidden sm:flex items-center gap-[5px] rounded p-2 border-r-[0.5px] border-r-gray-400 border-solid text-[#242424] sm:text-lg not-italic font-medium text-[12px]">
               <SmallHomeIcon />{" "}
               <div className="text-nowrap">All Residential</div>
             </div>
+
             <Fragment>
               <div className="flex justify-between items-center w-full">
                 <div className="flex sm:hidden items-center ">
@@ -59,7 +60,7 @@ function AppDrawer({ close, opened }: Props) {
                   <SearchSec />
                 </div>
 
-                <div className="flex gap-2">
+                <div className="hidden sm:flex gap-2">
                   <Nearme />
                   <a
                     href={`/search`}
@@ -68,9 +69,7 @@ function AppDrawer({ close, opened }: Props) {
                   >
                     {config.searchBtnIcon}
                   </a>
-
                 </div>
-                
 
               </div>
               {(f.city || f.locality.length > 0) && (
@@ -114,6 +113,18 @@ function AppDrawer({ close, opened }: Props) {
               )}
             </Fragment>
           </div>
+
+          {/* for mobile */}
+          <div className="sm:hidden mt-[10px] flex gap-2">
+            <Nearme />
+            <a
+              href={`/search`}
+              target="_blank"
+              className={`flex justify-center items-center gap-[4px] rounded p-1.5 md:p-2.5  text-white  text-[14px] 2xl:text-xl font-bold bg-[#0073c6]`}
+            >
+              {config.searchBtnIcon} Search
+            </a>
+          </div>
         </div>
         <div className="mt-2">
           <p className="text-[#242424] text-[14px] sm:text-xl not-italic font-medium leading-[normal] ">
@@ -152,7 +163,7 @@ const config = {
     </svg>
   ),
   searchIcon:(
-    <svg className="min-w-[14px] min-h-[14px] " xmlns="http://www.w3.org/2000/svg" width="14" height="15" viewBox="0 0 14 15" fill="none">
+    <svg className="hidden sm:flex min-w-[14px] min-h-[14px] " xmlns="http://www.w3.org/2000/svg" width="14" height="15" viewBox="0 0 14 15" fill="none">
       <path d="M11.6849 12.6267C11.9533 12.895 12.3674 12.4808 12.0991 12.2183L9.91161 10.025C10.6791 9.1761 11.1033 8.07196 11.1016 6.92751C11.1016 4.36668 9.01911 2.28418 6.45827 2.28418C3.89744 2.28418 1.81494 4.36668 1.81494 6.92751C1.81494 9.48835 3.89744 11.5708 6.45827 11.5708C7.61328 11.5708 8.68078 11.145 9.49744 10.4392L11.6849 12.6267ZM2.39769 6.92751C2.39769 4.68751 4.22352 2.86751 6.45769 2.86751C8.69769 2.86751 10.5177 4.68751 10.5177 6.92751C10.5177 9.16751 8.69769 10.9875 6.45769 10.9875C4.22352 10.9875 2.39769 9.16751 2.39769 6.92751Z" fill="#565D70"/>
     </svg>
   ),
