cf811d56b3d2f2ec40ca5c8858f9dacea49c23e1
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2024-08-11 20:24:28 +0530

    Update newtabCon.tsx

--- app/(dashboard)/search/components/leftsection/newtabCon.tsx
+++ app/(dashboard)/search/components/leftsection/newtabCon.tsx
@@ -1,30 +1,38 @@
 import React from "react";
 import SortBy from "./SortBy";
 
-const NewTabCon = ({ onTabChange, selectedProtype, categoryType, Activities }: any) => {
+const NewTabCon = ({
+  onTabChange,
+  selectedProtype,
+  categoryType,
+  Activities,
+}: any) => {
   return (
     <div className="flex flex-row justify-between items-center  align-middle flex-wrap sm:flex-nowrap max-w-full gap-[3px] md:gap-3 max-w-full  bg-[#e7f5ff] shadow-md  px-4 sm:px-[1px] md:px-4 md:py-2.5;">
-      <div className="flex flex-row w-[80%] sm:w-auto sm:w justify-between items-center  align-middle gap-1 md:gap-3 ">
+      <div className="flex flex-row w-[80%] sm:w-auto  justify-start items-center  gap-2   md:gap-3 ">
         {categoryType.map((item: any) => (
-          <button
-            onClick={() => onTabChange(item.value)}
-            className={` text-[12px] sm:text-[14px] font-[600] xl:text-base leading-0  font-montserrat cursor-pointer   ${
-              item.value == selectedProtype
-                ? "text-blue-500 underline font-semibold md:font-bold"
-                : "text-[#242424]"
-            }`}
-          >
-            {" "}
-            {item.label}
-          </button>
+          <>
+            <button
+              onClick={() => onTabChange(item.value)}
+              className={` text-[14px] sm:text-[14px] font-[600] xl:text-base leading-0  font-montserrat cursor-pointer text-nowrap   ${
+                item.value == selectedProtype
+                  ? "text-blue-500 underline font-semibold md:font-bold"
+                  : "text-[#242424]"
+              }`}
+            >
+              {" "}
+              {item.label}
+            </button>
+            <span>|</span>
+          </>
         ))}
       </div>
 
-      <div className="flex flex-row items-center gap-1 md:gap-2 self-end justify-center">
+      <div className="flex flex-row items-center gap-1 md:gap-2 self-end justify-center ml-auto">
         <h1 className="text-[12px] text-right sm:text-[14px] xl:text-base text-gray-400 font-semibold">
           Sort By:
         </h1>
-        <SortBy typeProp={selectedProtype}  ActivitiesType={Activities}/>
+        <SortBy typeProp={selectedProtype} ActivitiesType={Activities} />
       </div>
     </div>
   );
