62f1d9c7ea487f0197c647f13d1f2a1afaaf9ad7
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2024-08-13 13:54:39 +0530

    Create page.tsx

---
+++ app/(dashboard)/(contact_page)/contact/page.tsx
@@ -0,0 +1,17 @@
+import Footer from "@/app/components/layouts/primary/footer";
+import Header from "@/app/components/layouts/primary/header";
+import React from "react";
+
+type Props = {};
+
+export default function Page({}: Props) {
+  return (
+    <div>
+      <Header />{" "}
+      <div className="mt-[70px] w-[1395px] h-56 shrink-0 rounded-[10px] m-auto">
+        dfsdfsdfds
+      </div>{" "}
+      <Footer />
+    </div>
+  );
+}
