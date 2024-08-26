96eab182fd5483c7e7667c33b2d2cedeb2611aa0
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2024-08-26 16:24:44 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -39,7 +39,6 @@ const nextConfig = {
     APP_ENV: process.env.APP_ENV || process.env.NODE_ENV || "development",
   },
   staticPageGenerationTimeout: 180,
-  compress: true,
 };
 
 module.exports = nextConfig;
