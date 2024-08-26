6d0752d64578dfd85c76481a97060c9b363ddf15
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2024-08-26 11:22:40 +0530

    AGENT BUG FIXED

--- app/utils/parse.ts
+++ app/utils/parse.ts
@@ -19,10 +19,6 @@ interface GetValuesResult {
   ceoName: string;
 }
 
-
-
-
-
 function stateParser(originalData: any) {
   return originalData?.map((item: any) => ({
     value: JSON.stringify(item.cid),
@@ -49,22 +45,24 @@ function hideMobileNumber(mobile: number): string {
   const result = hiddenDigits + input.slice(-3);
   return result;
 }
-function getFoundedByName(arr:FoundedByItem[]): string {
-  if (arr.length > 1) {
+function getFoundedByName(arr: FoundedByItem[]): string {
+  if (arr?.length > 1) {
     // Extract and join names if array length is greater than 1
-    const names = arr.map(item => item.name).filter(name => name.trim() !== '');
-    return names.join(', ');
+    const names = arr
+      .map((item) => item.name)
+      .filter((name) => name.trim() !== "");
+    return names.join(", ");
   } else if (arr.length === 1) {
     // Return the name of the single item if array length is 1
     return arr[0].name.trim();
   }
   // Return an empty string if array length is 0
-  return '';
+  return "";
 }
 
 // Function to convert an array of names to a comma-separated string
 function arrayToCommaSeparatedString(names: string[]): string {
-  return names.filter(name => name.trim() !== '').join(', ');
+  return names.filter((name) => name.trim() !== "").join(", ");
 }
 
 // Function to get the values for foundedBy, managingDirectorNames, and ceoNames
@@ -76,20 +74,30 @@ function getValues({
   return {
     foundedBy: getFoundedByName(foundedBy),
     managingDirectorName: getFoundedByName(managingDirectorName),
-    ceoName: getFoundedByName(ceoName)
+    ceoName: getFoundedByName(ceoName),
   };
 }
 
 function registerOtherParser(data: any) {
-  const { otp, prevMobile, companyLogo, foundedBy, managingDirectorName, ceoName, ...parsedData } = data;
-  
-const items=getValues({foundedBy, managingDirectorName, ceoName})
-  
+  const {
+    otp,
+    prevMobile,
+    companyLogo,
+    foundedBy,
+    managingDirectorName,
+    ceoName,
+    ...parsedData
+  } = data;
+
+  const items =
+    foundedBy && managingDirectorName && ceoName
+      ? getValues({ foundedBy, managingDirectorName, ceoName })
+      : {};
 
   if (companyLogo) {
-    return {  ...parsedData, ...items,  isLogoChange: "Y", companyLogo };
+    return { ...parsedData, ...items, isLogoChange: "Y", companyLogo };
   } else {
-    return {...parsedData, ...items};
+    return { ...parsedData, ...items };
   }
 }
 
