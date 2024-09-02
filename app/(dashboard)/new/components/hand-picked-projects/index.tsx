167584bbbf98be5045d1d47f539ad9f588bfd628
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2024-09-02 10:32:57 +0530

    +++ DONE

--- app/(dashboard)/new/components/hand-picked-projects/index.tsx
+++ app/(dashboard)/new/components/hand-picked-projects/index.tsx
@@ -8,8 +8,12 @@ type Props = { data: any; shortIds: any };
 
 export default function HandPickedProjects({ data, shortIds }: Props) {
   const [active, setActive] = React.useState(0);
-  const url = 
-  active == 0 ? `${process.env.NEXT_PUBLIC_BACKEND_URL}/search?projStatus=108` : active == 1 ?` ${process.env.NEXT_PUBLIC_BACKEND_URL}/search?projStatus=106 `: `${process.env.NEXT_PUBLIC_BACKEND_URL}/search?projStatus=107` ;
+  const url =
+    active == 0
+      ? `${process.env.NEXT_PUBLIC_BACKEND_URL}/search?projStatus=108`
+      : active == 1
+      ? ` ${process.env.NEXT_PUBLIC_BACKEND_URL}/search?projStatus=106 `
+      : `${process.env.NEXT_PUBLIC_BACKEND_URL}/search?projStatus=107`;
 
   return (
     <div className="w-full mt-[40px] sm:mt-[80px] min-h-[530px] sm:min-h-[582px] flex justify-center items-center shrink-0 bg-gradient-to-r from-[#DAE6F1] via-[#DAE6F1] to-[#A7D0F5]">
@@ -19,7 +23,13 @@ export default function HandPickedProjects({ data, shortIds }: Props) {
             title="Handpicked Projects"
             content="Discover Your Dream Home with Handpicked Projects – Where Quality Meets Elegance"
           />
-            <a className="text-[#0073C6] text-nowrap font-[Montserrat] text-[14px] sm:text-[18px] xl:text-[20px] not-italic font-bold leading-[normal]" href={url}  target="_blank">View all</a>
+          <a
+            className="text-[#0073C6] text-nowrap font-[Montserrat] text-[14px] sm:text-[18px] xl:text-[20px] not-italic font-bold leading-[normal]"
+            href={url}
+            target="_blank"
+          >
+            View all
+          </a>
         </div>
 
         <div className=" w-full flex flex-col xl:flex-row justify-center items-start mt-2 xl:mt-10 flex-nowrap sm:gap-6 ">
@@ -27,7 +37,9 @@ export default function HandPickedProjects({ data, shortIds }: Props) {
 
           <div className=" max-w-[600px] sm:max-w-[1466px] mt-[16px] sm:mt-0 ">
             <CardCarousel
-              data={data.featured.filter((each:any)=> each.projstatus == config[active])}
+              data={data?.featured?.filter(
+                (each: any) => each.projstatus == config[active]
+              )}
               active={active}
               shortIds={shortIds}
             />
@@ -38,8 +50,4 @@ export default function HandPickedProjects({ data, shortIds }: Props) {
   );
 }
 
-const config = [
-  "New Launch",
-  "On Going",
-  "Completed",
-];
+const config = ["New Launch", "On Going", "Completed"];
