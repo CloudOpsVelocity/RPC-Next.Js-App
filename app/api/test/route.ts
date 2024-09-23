0d94be404bbdba5f1a5f37f0e2081420cdf8c5ea
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2024-09-23 10:32:26 +0530

    ++ LAT LONG DONE

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,13 +1,23 @@
-import fs from "fs";
 import { NextResponse } from "next/server";
-export async function GET(req: Request) {
+export async function GET() {
   try {
-    // const data = fs.appendFileSync("test.txt", `${new Date()}\n`, {
-    //   encoding: "utf-8",
-    // });
-    return NextResponse.json({ data: true });
+    const data = await getCityStateFromIP("192.168.45.123");
+    return NextResponse.json({ data, status: 200 });
   } catch (error) {
     console.error(error);
     return Response.json({ ok: false, error: "Error reading file" });
+  }
+}
+const axios = require("axios");
+
+async function getCityStateFromIP(ip: string) {
+  try {
+    const response = await axios.get(`https://get.geojs.io/v1/ip/geo.json`);
+    console.log(response);
+    const { city, region } = response.data;
+    return { city, state: region };
+  } catch (error) {
+    console.error("Error fetching IP data:", error);
+    return null;
   }
 }
