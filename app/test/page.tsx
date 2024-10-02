e524fb2f453996952f2d4fd3dac3d4cdb0640868
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2024-10-02 10:25:29 +0530

    Update page.tsx

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,42 +1,52 @@
+import { TbInnerShadowBottom } from "react-icons/tb";
 import EnhancedFooter from "./components/Section/Footer";
 
 export default async function Page() {
- class Node<T> {
-  public head:T;
-  public next:Node<T>|null;
-  constructor(value:T){
-   this.head = value;
-   this.next = null;
-  }
- }
- class LinkedList<T> {
-  public head:Node<T>;
-  private tail:Node<T>;
-  private length:number
-  constructor(value:T){
-    this.head = new Node(value);
-    this.tail = this.head;
-    this.length = 1
-  }
-  push(value:T){
-    let newNode = new Node(value)
-    this.tail.next = newNode
-    this.tail = newNode
-    this.length++
-  }
-  pop(){
-    if(this.length === 0) return 
-    
-  }
- }
- const list = new LinkedList('APPLE')
- list.push('ORANGE')
- list.push('MANGO')
- console.log({list})
+  // class Node<T> {
+  //   public head: T;
+  //   public next: Node<T> | null;
+  //   constructor(value: T) {
+  //     this.head = value;
+  //     this.next = null;
+  //   }
+  // }
+  // class LinkedList<T> {
+  //   public head: Node<T>;
+  //   private tail: Node<T>;
+  //   private length: number;
+  //   constructor(value: T) {
+  //     this.head = new Node(value);
+  //     this.tail = this.head;
+  //     this.length = 1;
+  //   }
+  //   push(value: T) {
+  //     let newNode = new Node(value);
+  //     this.tail.next = newNode;
+  //     this.tail = newNode;
+  //     this.length++;
+  //   }
+  //   pop() {
+  //     if (this.length === 0) return;
+  //     let temp = this.head;
+  //     let prev = this.head
+  //     while (temp.next) {
+  //       prev = temp
+  //       temp = prev.next
+  //     }
+  //     this.tail = prev;
+  //     this.tail = null;
+  //     this.length --
+  //     return temp
+  //   }
+  // }
+  // const list = new LinkedList("APPLE");
+  // list.push("ORANGE");
+  // list.push("MANGO");
+  // list.pop()
+  // console.log({ list });
   return (
     <div>
       <EnhancedFooter />
     </div>
   );
 }
-
