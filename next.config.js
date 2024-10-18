e1b52c36a7c6039c441b34f9fc3d5a78e1d2f572
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2024-10-18 19:34:57 +0530

    NEW ADDED

--- next.config.js
+++ next.config.js
@@ -19,6 +19,7 @@ const nextConfig = {
       { hostname: "d1l03fubsuphsh.cloudfront.net" },
       { hostname: "d2l0lb5gc1bw3t.cloudfront.net" },
       { hostname: "daxv8eiot4y5y.cloudfront.net" },
+      {hostname:"media.getrightproperty.com"},
       {
         hostname:
           "getrightproperty-test-bucket-new.s3.ap-south-1.amazonaws.com",
