926e88accefcce234e09a020e1a1b5e5dda6cb86
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2024-11-28 16:12:45 +0530

    ++ DONE

--- app/(dashboard)/new/components/home-search/SearchSec.tsx
+++ app/(dashboard)/new/components/home-search/SearchSec.tsx
@@ -75,7 +75,7 @@ export default function SearchSec({}: Props) {
             onClick={handleFieldClick}
             value={name ?? ""}
             onChange={(e) => onSearchChange(e.target.value)}
-            className="min-w-[255px]  text-[14px] h-[62px] outline-none pr-2 py-1 placeholder:text-gray-600"
+            className="min-w-[255px]  text-[12px] sm:text-[14px] outline-none pr-2 py-1 placeholder:text-gray-600 ios-zoom-fix"
           />
         </div>
       </Combobox.Target>
