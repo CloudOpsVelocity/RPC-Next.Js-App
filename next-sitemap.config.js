05dc62bd5ea01a4d4810ddd5883b526bade4eb92
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2024-12-06 13:55:36 +0530

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'http://localhost:3000' || 'https://example.com',
+    siteUrl: process.env.NEXT_PUBLIC_URL || 'https://example.com',
     exclude: ['/icon.svg', '/apple-icon.png', '/manifest.webmanifest', '/tags/*'],
     generateIndexSitemap: false,
     // generateRobotsTxt: true,
