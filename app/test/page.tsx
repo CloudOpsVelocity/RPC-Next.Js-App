97542badd40ee8e016819c11cd4a8e4d84da2009
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2024-12-12 15:09:13 +0530

    Update page.tsx

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -7,35 +7,33 @@ import Carousel from "./components/NewCarousel";
 type Props = {};
 
 export default function Page({}: Props) {
-const getData = (array:number[]) =>{
-      for (let i = 0; i < array.length; i++) {
-for (let j = 0; j < array.length; j++) {
-  const element = array[j];
-  
-}
+  const getData = (array: number[]) => {
+    for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < array.length; j++) {
+        const element = array[j];
       }
-    return array
-}
-console.log(getData([3,5,1,58,765,4,343,23,65]))
-  return (
- <CoverImage />
-  );
+    }
+    return array;
+  };
+  console.log(getData([3, 5, 1, 58, 765, 4, 343, 23, 65]));
+  return <CoverImage />;
 }
 
-import Image from 'next/image';
+import Image from "next/image";
 
 // Your coverImageUrl string
-const coverImageUrl = "https://d2l0lb5gc1bw3t.cloudfront.net/residential-projects/bengaluru/213/hfhdhfgf-a-narayanapura-cover.webp?v=1730784410064,https://d2l0lb5gc1bw3t.cloudfront.net/residential-projects/bengaluru/213/hfhdhfgf-a-narayanapura-cover-small.webp,https://d2l0lb5gc1bw3t.cloudfront.net/residential-projects/bengaluru/213/hfhdhfgf-a-narayanapura-cover-medium.webp,https://d2l0lb5gc1bw3t.cloudfront.net/residential-projects/bengaluru/213/hfhdhfgf-a-narayanapura-cover-large.webp";
+const coverImageUrl =
+  "https://d2l0lb5gc1bw3t.cloudfront.net/residential-projects/bengaluru/213/hfhdhfgf-a-narayanapura-cover.webp?v=1730784410064,https://d2l0lb5gc1bw3t.cloudfront.net/residential-projects/bengaluru/213/hfhdhfgf-a-narayanapura-cover-small.webp,https://d2l0lb5gc1bw3t.cloudfront.net/residential-projects/bengaluru/213/hfhdhfgf-a-narayanapura-cover-medium.webp,https://d2l0lb5gc1bw3t.cloudfront.net/residential-projects/bengaluru/213/hfhdhfgf-a-narayanapura-cover-large.webp";
 
 // Split the URLs into an array
-const [defaultImg, smallImg, mediumImg, largeImg] = coverImageUrl.split(',');
+const [defaultImg, smallImg, mediumImg, largeImg] = coverImageUrl.split(",");
 
- function CoverImage() {
+function CoverImage() {
   return (
     <>
-    {/* // <picture>
+      {/* // <picture>
     //   <source */}
-    {/* //     media="(max-width: 660px)"
+      {/* //     media="(max-width: 660px)"
     //     srcSet={smallImg}
     //   />
     //   <source
@@ -46,14 +44,17 @@ const [defaultImg, smallImg, mediumImg, largeImg] = coverImageUrl.split(',');
     //     media="(min-width: 2000px)"
     //     srcSet={largeImg}
     //   /> */}
-      <Image
-        src={defaultImg}
-        alt="Project cover image"
-        width={800}
-        height={600}
-
-        // sizes="(max-width: 320px) 100vw, (max-width: 768px) 100vw, (max-width: 1280px) 100vw, (max-width: 3840px) 100vw"
-      />
+      <button
+        onClick={() =>
+          navigator.share({
+            title: "Share",
+            text: "Share",
+            url: "https://www.google.com",
+          })
+        }
+      >
+        Share
+      </button>
     </>
   );
 }
