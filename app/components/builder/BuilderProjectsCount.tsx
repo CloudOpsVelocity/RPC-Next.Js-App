874b5053804ba538450743b685de5758be6cfb80
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2024-12-17 12:38:48 +0530

    Update BuilderProjectsCount.tsx

--- app/components/builder/BuilderProjectsCount.tsx
+++ app/components/builder/BuilderProjectsCount.tsx
@@ -41,7 +41,7 @@ export default function BuilderProjectsCount({ id }: Props) {
   });
   if (isLoading) {
     return (
-      <div className="sm:rounded-[20px] mt-[3%] flex justify-between items-center bg-[#FFF] shadow-md w-[100%] mb-[3%] sm:w-[65%] xl:w-[50%] p-[1%] border border-gray-300">
+      <div className="sm:rounded-[20px]  flex justify-between items-center bg-[#FFF] shadow-md w-[100%] mb-[3%] sm:w-[65%] xl:w-[50%] p-[1%] border border-gray-300">
         <div className="w-[30%] border-solid border-[#92B2C8] border-r-[1px] animate-pulse">
           <div className="h-8 bg-gray-200 rounded w-3/4 mb-2" />
           <div className="h-6 bg-gray-200 rounded w-full" />
@@ -59,69 +59,86 @@ export default function BuilderProjectsCount({ id }: Props) {
   }
 
   return (
-    <div className="sm:rounded-[20px] mt-[3%] flex justify-between items-center bg-[#FFF] shadow-md w-[100%] mb-[3%]   sm:w-[65%] xl:w-[50%] p-[1%] border border-gray-300">
+    <div className="sm:rounded-[20px] flex justify-between items-center bg-[#FFF] shadow-md w-[100%] mb-[1%] sm:w-[65%] xl:w-[50%] p-[1%] border border-gray-300">
       <div className="w-[30%] border-solid border-[#92B2C8] border-r-[1px]">
-        <div className="flex justify-between items-center w-[90%] ">
-          <span className=" text-[#202020]text-[16px]  sm:text-[20px] lg:text-[24px] font-[600]">
-            {data?.newLaunch ?? 0}
-          </span>
-          {newLaunchProjIconSvg}
-        </div>
-
         {data?.newLaunch > 0 ? (
-          <a
-            href={`/search?projStatus=108`}
-            className=" text-[#148B16] text-[13px] lg:text-[20px] font-[700] "
-          >
-            New Launch {`Project${data?.newLaunch > 1 ? "s" : ""}`}
+          <a href={`/search?projStatus=108`} className="block">
+            <div className="flex justify-between items-center w-[90%]">
+              <span className="text-[#202020]text-[16px] sm:text-[20px] lg:text-[24px] font-[600]">
+                {data?.newLaunch}
+              </span>
+              {newLaunchProjIconSvg}
+            </div>
+            <span className="text-[#148B16] text-[13px] lg:text-[20px] font-[700]">
+              New Launch {`Project${data?.newLaunch > 1 ? "s" : ""}`}
+            </span>
           </a>
         ) : (
-          <p className=" text-[#148B16] text-[13px] lg:text-[20px] font-[700] ">
-            New Launch Projects
-          </p>
+          <div>
+            <div className="flex justify-between items-center w-[90%]">
+              <span className="text-[#202020]text-[16px] sm:text-[20px] lg:text-[24px] font-[600]">
+                0
+              </span>
+            </div>
+            <p className="text-[#148B16] text-[13px] lg:text-[20px] font-[700]">
+              New Launch Projects
+            </p>
+          </div>
         )}
       </div>
 
       <div className="w-[30%] border-solid border-[#92B2C8] border-r-[1px]">
-        <div className="flex justify-between items-center w-[90%] ">
-          <span className=" text-[#202020]text-[16px]  sm:text-[20px] lg:text-[24px] font-[600]">
-            {data?.onGoing ?? 0}
-          </span>
-          {onGoingProjIconSvg}
-        </div>
         {data?.onGoing > 0 ? (
-          <a
-            href={`/search?projStatus=106`}
-            className=" text-[#0073C6] text-[13px] lg:text-[20px] font-[700]"
-          >
-            Ongoing <br className="block sm:hidden" />{" "}
-            {`Project${data?.onGoing > 1 ? "s" : ""}`}
+          <a href={`/search?projStatus=106`} className="block">
+            <div className="flex justify-between items-center w-[90%]">
+              <span className="text-[#202020]text-[16px] sm:text-[20px] lg:text-[24px] font-[600]">
+                {data?.onGoing}
+              </span>
+              {onGoingProjIconSvg}
+            </div>
+            <span className="text-[#0073C6] text-[13px] lg:text-[20px] font-[700]">
+              Ongoing <br className="block sm:hidden" />{" "}
+              {`Project${data?.onGoing > 1 ? "s" : ""}`}
+            </span>
           </a>
         ) : (
-          <p className=" text-[#0073C6] text-[13px] lg:text-[20px] font-[700]">
-            Ongoing <br className="block sm:hidden" /> Projects
-          </p>
+          <div>
+            <div className="flex justify-between items-center w-[90%]">
+              <span className="text-[#202020]text-[16px] sm:text-[20px] lg:text-[24px] font-[600]">
+                0
+              </span>
+            </div>
+            <p className="text-[#0073C6] text-[13px] lg:text-[20px] font-[700]">
+              Ongoing <br className="block sm:hidden" /> Projects
+            </p>
+          </div>
         )}
       </div>
 
-      <div className="w-[30%] ">
-        <div className="flex justify-between items-center w-[90%] ">
-          <span className=" text-[#202020]text-[16px]  sm:text-[20px] lg:text-[24px] font-[600]">
-            {data?.completed ?? 0}
-          </span>
-          {completedProjIconSvg}
-        </div>
+      <div className="w-[30%]">
         {data?.completed > 0 ? (
-          <a
-            href={`/search?projStatus=107`}
-            className=" text-[#E3AC00] text-[13px] lg:text-[20px] font-[700]"
-          >
-            Completed {`Project${data?.completed > 1 ? "s" : ""}`}
+          <a href={`/search?projStatus=107`} className="block">
+            <div className="flex justify-between items-center w-[90%]">
+              <span className="text-[#202020]text-[16px] sm:text-[20px] lg:text-[24px] font-[600]">
+                {data?.completed}
+              </span>
+              {completedProjIconSvg}
+            </div>
+            <span className="text-[#E3AC00] text-[13px] lg:text-[20px] font-[700]">
+              Completed {`Project${data?.completed > 1 ? "s" : ""}`}
+            </span>
           </a>
         ) : (
-          <p className=" text-[#E3AC00] text-[13px] lg:text-[20px] font-[700]">
-            Completed Projects
-          </p>
+          <div>
+            <div className="flex justify-between items-center w-[90%]">
+              <span className="text-[#202020]text-[16px] sm:text-[20px] lg:text-[24px] font-[600]">
+                0
+              </span>
+            </div>
+            <p className="text-[#E3AC00] text-[13px] lg:text-[20px] font-[700]">
+              Completed Projects
+            </p>
+          </div>
         )}
       </div>
     </div>
