37cded278b052cbb9e3d6bb0106bbf080d4d97a4
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2025-01-06 19:53:49 +0530

    ++ FLOOR PLAN UPDATED

--- app/components/project/newFloorPlan/floor-plan.tsx
+++ app/components/project/newFloorPlan/floor-plan.tsx
@@ -36,6 +36,9 @@ import { BiBuildingHouse } from "react-icons/bi";
 import { LuBedDouble } from "react-icons/lu";
 import Image from "next/image";
 import { ImgNotAvail } from "@/app/data/project";
+import { FaBed } from "react-icons/fa6";
+import { MdBalcony } from "react-icons/md";
+import PropertyCard from "./components/property-card";
 const iconStyles: string =
   " flex items-center justify-center w-[34px] sm:w-[40px] h-[34px] sm:h-[40px] bg-[#FAFDFF] rounded-[50%] ";
 const propertyUnits: PropertyUnit[] = [
@@ -296,47 +299,71 @@ export default function FloorPlans({
             ) : (
               projectUnitsData &&
               projectUnitsData.map((unit: any) => (
+                // <PropertyCard
+                //   key={unit.unitId}
+                //   bhk="2 BHK"
+                //   type="Apartment"
+                //   tower="B"
+                //   floor="2"
+                //   unit="121"
+                //   area={{
+                //     value: 1211,
+                //     unit: "sq.ft",
+                //   }}
+                //   facing="North"
+                //   details={{
+                //     bedrooms: 2,
+                //     bathrooms: 2,
+                //     balconies: 1,
+                //   }}
+                //   onViewDetails={() => console.log("View details clicked")}
+                // />
                 <button
                   onClick={() => setModalState({ isOpen: true, unit })}
-                  className="w-full bg-white rounded-xl border-2 border-gray-200 p-6 transition-all hover:border-[#0073C6] hover:shadow-lg group"
+                  className="w-full  rounded-xl border-2 border-gray-200 p-4 transition-all hover:border-[#0073C6] hover:shadow-xl group from-[#F8FAFC] to-white"
                   key={unit.unitId}
                 >
                   {/* Header Section */}
-                  <div className="flex items-start justify-between gap-4 border-b border-gray-100">
-                    <div className="flex-1">
-                      <div className="flex items-center gap-2 mb-1">
-                        <h3 className="text-2xl font-bold text-[#0073C6] text-nowrap">
+                  <div className="flex flex-col sm:flex-row items-start justify-between gap-4 border-b border-gray-100 pb-4">
+                    <div className="flex-1 w-full sm:w-auto">
+                      <div className="flex flex-wrap items-center gap-2 mb-2">
+                        <h3 className="text-2xl sm:text-2xl font-bold text-[#232323]">
                           {unit.bhkName}
                         </h3>
                         <span className="px-3 py-1 text-sm font-semibold bg-blue-50 text-[#0073C6] rounded-full">
                           {unit.aptTypeName}
                         </span>
                       </div>
-                      <div className="flex items-center gap-2 text-gray-600">
+                      <div className="flex flex-wrap items-center gap-2 text-gray-600 text-base sm:text-lg">
                         {unit.towerName && (
-                          <p className="text-sm font-medium font-bold">
-                            {unit.towerName}
-                          </p>
+                          <div className="flex items-center bg-gray-100 rounded-full px-3 py-1">
+                            <FaBuilding className="text-[#0073C6] mr-2" />
+                            <p className="font-semibold">{unit.towerName}</p>
+                          </div>
                         )}
                         {unit.floor && (
-                          <p className="text-sm font-medium font-bold">
-                            - Floor {unit.floor}
-                          </p>
+                          <div className="flex items-center bg-gray-100 rounded-full px-3 py-1">
+                            <BiBuildingHouse className="text-[#0073C6] mr-2" />
+                            <p className="font-semibold">Floor {unit.floor}</p>
+                          </div>
                         )}
                         {unit.unitNumber && (
-                          <p className="text-sm font-medium font-bold">
-                            - Unit {unit.unitNumber}
-                          </p>
+                          <div className="flex items-center bg-gray-100 rounded-full px-3 py-1">
+                            <FaBed className="text-[#0073C6] mr-2" />
+                            <p className="font-semibold">
+                              Unit {unit.unitNumber}
+                            </p>
+                          </div>
                         )}
                       </div>
                     </div>
-                    <div className="grid grid-cols-2 gap-6 mb-6">
+                    <div className="flex flex-wrap justify-between w-full sm:w-auto gap-4 mt-4 sm:mt-0">
                       {unit.superBuildUparea && (
                         <div className="space-y-1">
                           <p className="text-gray-500 text-sm font-medium">
                             Super Built-up Area
                           </p>
-                          <p className="text-gray-900 text-lg font-bold">
+                          <p className="text-gray-900 text-xl font-bold">
                             {unit.superBuildUparea} sq.ft
                           </p>
                         </div>
@@ -344,43 +371,42 @@ export default function FloorPlans({
                       {unit.facingName && (
                         <div className="space-y-1">
                           <div className="flex items-center gap-1">
-                            <FaCompass className="text-[#0073C6] text-sm" />
+                            <FaCompass className="text-[#0073C6] text-lg" />
                             <p className="text-gray-500 text-sm font-medium">
                               Facing
                             </p>
                           </div>
-                          <p className="text-gray-900 text-lg font-bold">
+                          <p className="text-gray-900 text-xl font-bold">
                             {unit.facingName}
                           </p>
                         </div>
                       )}
                     </div>
-                    <FaArrowRight className="text-[#0073C6] text-xl opacity-0 group-hover:opacity-100 group-hover:translate-x-1 transition-all" />
                   </div>
 
                   {/* Additional Details */}
-                  <div className="grid grid-cols-4 gap-4 mb-6 bg-gray-50 rounded-lg p-4">
-                    {unit?.bhkName && (
+                  <div className="grid grid-cols-2 sm:grid-cols-4 gap-4 bg-gray-50 rounded-lg p-4 ">
+                    {unit.bhkName && (
                       <div className="flex items-center gap-2">
-                        <LuBedDouble className="text-[#0073C6] text-lg" />
+                        <FaBed className="text-[#0073C6] text-2xl" />
                         <div>
-                          <p className="text-xs text-gray-500 font-medium">
+                          <p className="text-sm text-gray-500 font-medium">
                             Bedrooms
                           </p>
-                          <p className="text-sm font-bold">
-                            {unit?.bhkName?.split(" ")[0]}
+                          <p className="text-lg font-bold">
+                            {unit.bhkName.split(" ")[0]}
                           </p>
                         </div>
                       </div>
                     )}
                     {unit.totalNumberofBathroom && (
                       <div className="flex items-center gap-2">
-                        <FaBath className="text-[#0073C6] text-lg" />
+                        <FaBath className="text-[#0073C6] text-2xl" />
                         <div>
-                          <p className="text-xs text-gray-500 font-medium">
+                          <p className="text-sm text-gray-500 font-medium">
                             Bathrooms
                           </p>
-                          <p className="text-sm font-bold">
+                          <p className="text-lg font-bold">
                             {unit.totalNumberofBathroom}
                           </p>
                         </div>
@@ -388,30 +414,38 @@ export default function FloorPlans({
                     )}
                     {unit.totalNumberOfBalcony && (
                       <div className="flex items-center gap-2">
-                        <BiBuildingHouse className="text-[#0073C6] text-lg" />
+                        <MdBalcony className="text-[#0073C6] text-2xl" />
                         <div>
-                          <p className="text-xs text-gray-500 font-medium">
+                          <p className="text-sm text-gray-500 font-medium">
                             Balconies
                           </p>
-                          <p className="text-sm font-bold">
+                          <p className="text-lg font-bold">
                             {unit.totalNumberOfBalcony}
                           </p>
                         </div>
                       </div>
                     )}
-                    {unit.terraceArea && (
+                    {unit.terraceArea && unit.terraceArea !== "null" && (
                       <div className="flex items-center gap-2">
-                        <BiBuildingHouse className="text-[#0073C6] text-lg" />
+                        <BiBuildingHouse className="text-[#0073C6] text-2xl" />
                         <div>
-                          <p className="text-xs text-gray-500 font-medium">
+                          <p className="text-sm text-gray-500 font-medium">
                             Terrace Area
                           </p>
-                          <p className="text-sm font-bold">
+                          <p className="text-lg font-bold">
                             {unit.terraceArea} sq.ft
                           </p>
                         </div>
                       </div>
                     )}
+                  </div>
+
+                  {/* View Details Button */}
+                  <div className="mt-4 text-right">
+                    <span className="inline-flex items-center text-[#0073C6] font-semibold group-hover:underline">
+                      View Details
+                      <FaArrowRight className="ml-2 group-hover:translate-x-1 transition-transform" />
+                    </span>
                   </div>
                 </button>
               ))
