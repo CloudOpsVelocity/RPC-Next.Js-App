5709df00c533abf20a75932cac9393e4e77f529e
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2025-01-10 17:35:33 +0530

    ++ NEW CAHAGES DONE

--- app/(new_routes_seo)/(project-listing-route)/residential-projects/[cg]/[city]/[lt]/[project]/[phase]/[bhk_unit_type]/page.tsx
+++ app/(new_routes_seo)/(project-listing-route)/residential-projects/[cg]/[city]/[lt]/[project]/[phase]/[bhk_unit_type]/page.tsx
@@ -85,7 +85,6 @@ export default async function Page({ params }: Props) {
       totalPrice,
     };
   }
-
   return !isProjectListing ? (
     <NewListingSearchpage
       serverData={serverData}
--- app/(new_routes_seo)/search/listing/components/listingSearchTabs/listingSearchRightSection.tsx
+++ app/(new_routes_seo)/search/listing/components/listingSearchTabs/listingSearchRightSection.tsx
@@ -9,6 +9,7 @@ import { useInfiniteQuery } from "react-query";
 import { getSearchData } from "../../../utils/project-search-queryhelpers";
 import { useQueryState } from "nuqs";
 import RTK_CONFIG from "@/app/config/rtk";
+import { usePathname } from "next/navigation";
 
 const ListingSearchRightSection = ({ serverData }: any) => {
   const Map = useMemo(
@@ -22,18 +23,17 @@ const ListingSearchRightSection = ({ serverData }: any) => {
       ),
     []
   );
-
+  const pathname = usePathname();
   const [apiFilterQueryParams] = useQueryState("sf");
-  let isTrue = serverData !== null || apiFilterQueryParams !== null;
+  let isTrue = pathname.includes("search")
+    ? true
+    : serverData !== null && apiFilterQueryParams !== null;
   const { data, isLoading, hasNextPage, fetchNextPage, refetch } =
     useInfiniteQuery({
       queryKey: [
         `searchQuery${apiFilterQueryParams ? `-${apiFilterQueryParams}` : ""}`,
       ],
       queryFn: async ({ pageParam = 0 }) => {
-        if (serverData && !isTrue) {
-          return serverData;
-        }
         const response = await getSearchData(
           pageParam,
           apiFilterQueryParams ? apiFilterQueryParams : ""
@@ -51,6 +51,7 @@ const ListingSearchRightSection = ({ serverData }: any) => {
       ...RTK_CONFIG,
       enabled: false,
     });
+    const apidata = !isTrue ? serverData : data?.pages.flat() || []
   return (
     <div
       className="w-[100%] sm:w-[50%]  flex justify-start items-start z-[1] md:w-[50%] scroll-mt-[150px]"
@@ -58,9 +59,9 @@ const ListingSearchRightSection = ({ serverData }: any) => {
     >
       <Map
         projName={"Searched Location"}
-        lat={(data && data?.pages.flat()[0]?.lat) ?? 47.46489}
-        lang={(data && data?.pages.flat()[0]?.lang) ?? 15.34043}
-        data={data?.pages.flat()}
+        lat={(apidata && apidata[0]?.lat) ?? 47.46489}
+         lang={(apidata && apidata[0]?.lang) ?? 15.34043}
+        data={apidata}
         type={"prop"}
       />
     </div>
