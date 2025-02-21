c5206bff42158a4632a1f08a8d0ffeea7df1f847
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2025-02-21 19:38:05 +0530

    ++ DONE

--- app/(new_routes_seo)/search/listing/NewListingSearchpage.tsx
+++ app/(new_routes_seo)/search/listing/NewListingSearchpage.tsx
@@ -5,6 +5,7 @@ import ListingSearchRightSection from "./components/listingSearchTabs/listingSea
 import ProjectSearchBreadCrumbs from "../components/ProjSearchBreadCrums";
 
 import { ListingSearchSchema } from "@/app/seo/search/listing-search.schema";
+import ListingHeaderFilters from "./components/ListingSearchHeader";
 
 type Props = {
   serverData: any;
@@ -26,9 +27,9 @@ export default function NewListingSearchpage({
       {serverData && <ListingSearchSchema properties={serverData} />}
       <div className="relative md:fixed top-0 md:top-[70px] z-auto md:z-10 w-full ">
         <ProjectSearchBreadCrumbs pageUrl={pageUrl} />
-        <ProjSearchMainFilterSection
-          isListing={serverData === null ? true : false}
-        />
+        <div className="flex flex-row items-start gap-2">
+          <ListingHeaderFilters isListing={true} />
+        </div>
       </div>
       <div className="sm:min-w-full xl:m-0 flex justify-between items-start flex-wrap-reverse sm:flex-nowrap relative md:pt-[154px] xl:pt-[190px] ">
         <ListingSearhLeftSection
