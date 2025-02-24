6689d8ffa32431283cbac381f87859028d407411
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2025-02-24 19:32:37 +0530

    Create home.schema.ts

---
+++ app/seo/common/home.schema.ts
@@ -0,0 +1,25 @@
+const homeLinksData = [
+  {
+    name: "Home",
+    url: "/",
+  },
+  {
+    name: "Post Property",
+    url: "/post-property",
+  },
+  {
+    name: "Post Project",
+    url: "/post-project",
+  },
+  { name: "About", url: "/about" },
+  { name: "Your Best Real Estate Guide", url: "/contact" },
+  { name: "Terms", url: "/terms" },
+  { name: "Privacy", url: "/privacy" },
+  { name: "Sitemap", url: "/sitemap" },
+];
+export const homeSiteNavigationSchema = {
+  "@context": "https://schema.org",
+  "@type": "SiteNavigationElement",
+  name: "Home",
+  url: "/",
+};
