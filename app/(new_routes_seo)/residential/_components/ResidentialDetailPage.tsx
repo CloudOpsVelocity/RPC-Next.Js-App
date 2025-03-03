726f53b0408afa13563146a321d79e60400734dd
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2025-03-03 12:26:01 +0530

    Update ResidentialDetailPage.tsx

--- app/(new_routes_seo)/residential/_components/ResidentialDetailPage.tsx
+++ app/(new_routes_seo)/residential/_components/ResidentialDetailPage.tsx
@@ -199,7 +199,7 @@ export default function ResidentialPage({ data }: { data: any }) {
         </div>
       </section>
 
-      {/* <section className="py-20 bg-muted/30">
+      <section className="py-20 bg-muted/30">
         <div className="container mx-auto">
           <h2 className="text-3xl font-bold text-center mb-16">
             What Our Customers Say
@@ -234,7 +234,7 @@ export default function ResidentialPage({ data }: { data: any }) {
           </div>
         </div>
       </section>
-
+      {/* 
       <section className="py-16 bg-primary text-white">
         <div className="container mx-auto text-center">
           <h2 className="text-3xl font-bold mb-4">
