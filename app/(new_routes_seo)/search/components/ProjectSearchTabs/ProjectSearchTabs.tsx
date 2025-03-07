1ac2a03a6c49aabb2893c46da6334531e87b04dc
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2025-03-07 14:01:30 +0530

    Update ProjectSearchTabs.tsx

--- app/(new_routes_seo)/search/components/ProjectSearchTabs/ProjectSearchTabs.tsx
+++ app/(new_routes_seo)/search/components/ProjectSearchTabs/ProjectSearchTabs.tsx
@@ -1,14 +1,13 @@
 "use client";
 
 import { useAtom, useSetAtom } from "jotai";
-import React, { useEffect } from "react";
+import React from "react";
 import {
   diffToProjFromListing,
   initialState,
   projSearchStore,
 } from "../../store/projSearchStore";
 import useProjSearchAppliedFilters from "../../hooks/useProjSearchAppliedFilters";
-import { update } from "lodash";
 import { SearchFilter } from "@/app/types/search";
 import selectedSearchAtom, { selectedNearByAtom } from "@/app/store/search/map";
 
@@ -178,9 +177,9 @@ export default function ProjectSearchTabs() {
           <div
             ref={scrollContainerRef}
             onWheel={handleWheel}
-            className="overflow-x-auto no-scrollbar max-w-full pb-[2px] "
+            className="md:overflow-x-auto md:no-scrollbar max-w-full pb-[2px] "
           >
-            <div className="flex items-center sm:gap-1 sm:p-0 xl:gap-2 min-w-max pb-[4px] ">
+            <div className="flex flex-wrap items-center  sm:gap-1 sm:p-0 xl:gap-2 sm:min-w-max pb-[4px] ">
               {tabs.map((tab) => (
                 <button
                   key={tab.id}
@@ -196,6 +195,61 @@ export default function ProjectSearchTabs() {
                   {tab.label}
                 </button>
               ))}
+              <div className=" relative flex md:hidden justify-end self-end  ">
+                <button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setIsDropdownOpen(!isDropdownOpen);
+                  }}
+                  className="flex items-center gap-2 px-[6px] py-[4px] xl:px-4 xl:py-2 text-sm md:text-base text-black hover:text-white hover:bg-[#0073C6] rounded-full transition-colors"
+                >
+                  <svg
+                    className="w-4 h-4"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M7 16V4m0 0L3 8m4-4l4 4m-4 8v-4m10 4v-4m0 4l-4-4m4 4l4-4"
+                    />
+                  </svg>
+
+                  <div className="max-w-[105px] overflow-hidden text-ellipsis whitespace-nowrap">
+                    {/*  {sortOptions.find((option) => option.value === sortBy)?.label ||
+                  "Sort By"} */}
+                    {state.sortByfield != null && state.sortType != null
+                      ? getSortyByValue(state)
+                      : "Newest First"}
+                  </div>
+                </button>
+
+                {isDropdownOpen && (
+                  <div className="absolute top-[40px] right-0 w-48 bg-white rounded-lg shadow-lg py-1 z-20 border border-white">
+                    {sortOptions.map((option) => (
+                      <button
+                        key={option.value}
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          // setSortBy(option.value);
+                          handleSortBy(option);
+                          setIsDropdownOpen(false);
+                        }}
+                        className={`block w-full text-left px-4 py-2 text-sm transition-colors ${
+                          getSortyByValue(state) === option.label
+                            ? "text-white bg-[#0073C6]"
+                            : "text-gray-700 hover:bg-[#0073C6] hover:text-white"
+                        }
+                    `}
+                      >
+                        {option.label}
+                      </button>
+                    ))}
+                  </div>
+                )}
+              </div>
             </div>
           </div>
 
@@ -257,62 +311,6 @@ export default function ProjectSearchTabs() {
             )}
           </div>
         </div>
-      </div>
-
-      <div className=" relative flex md:hidden justify-end self-end bg-slate-50 shadow-md pb-[4px] ">
-        <button
-          onClick={(e) => {
-            e.stopPropagation();
-            setIsDropdownOpen(!isDropdownOpen);
-          }}
-          className="flex items-center gap-2 px-[6px] py-[4px] xl:px-4 xl:py-2 text-sm md:text-base text-black hover:text-white hover:bg-[#0073C6] rounded-full transition-colors"
-        >
-          <svg
-            className="w-4 h-4"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M7 16V4m0 0L3 8m4-4l4 4m-4 8v-4m10 4v-4m0 4l-4-4m4 4l4-4"
-            />
-          </svg>
-
-          <div className="max-w-[105px] overflow-hidden text-ellipsis whitespace-nowrap">
-            {/*  {sortOptions.find((option) => option.value === sortBy)?.label ||
-                  "Sort By"} */}
-            {state.sortByfield != null && state.sortType != null
-              ? getSortyByValue(state)
-              : "Newest First"}
-          </div>
-        </button>
-
-        {isDropdownOpen && (
-          <div className="absolute top-[40px] right-0 w-48 bg-white rounded-lg shadow-lg py-1 z-20 border border-white">
-            {sortOptions.map((option) => (
-              <button
-                key={option.value}
-                onClick={(e) => {
-                  e.stopPropagation();
-                  // setSortBy(option.value);
-                  handleSortBy(option);
-                  setIsDropdownOpen(false);
-                }}
-                className={`block w-full text-left px-4 py-2 text-sm transition-colors ${
-                  getSortyByValue(state) === option.label
-                    ? "text-white bg-[#0073C6]"
-                    : "text-gray-700 hover:bg-[#0073C6] hover:text-white"
-                }
-                    `}
-              >
-                {option.label}
-              </button>
-            ))}
-          </div>
-        )}
       </div>
     </div>
   );
