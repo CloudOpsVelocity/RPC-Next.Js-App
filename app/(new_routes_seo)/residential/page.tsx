daa8733b95d2028fdf940acb423a1e9d1e5754a1
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2025-03-07 17:17:05 +0530

    Update page.tsx

--- app/(new_routes_seo)/residential/page.tsx
+++ app/(new_routes_seo)/residential/page.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import ResidentialPage from "./_components/ResidentialDetailPage";
 import axios from "axios";
+import { redirect } from "next/navigation";
 
 type Props = {};
 
 export default async function page({}: Props) {
-  let url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/home/page/project?city=9`;
-  try {
-    const data = await axios.get(url);
-    return <ResidentialPage data={data?.data} />;
-  } catch (error) {
-    console.log(error);
-    return <div>Error</div>;
-  }
+  return redirect("/");
+  // let url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/home/page/project?city=9`;
+  // try {
+  //   const data = await axios.get(url);
+  //   return <ResidentialPage data={data?.data} />;
+  // } catch (error) {
+  //   console.log(error);
+  //   return <div>Error</div>;
+  // }
 }
