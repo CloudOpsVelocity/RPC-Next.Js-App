172b7e3ac2d5b37f74586a366070efca841f4226
rpc-vinay <131428813+rpc-vinay@users.noreply.github.com>
2025-03-13 17:27:45 +0530

    cc

--- app/(dashboard)/new/components/PostProjectBtn.tsx
+++ app/(dashboard)/new/components/PostProjectBtn.tsx
@@ -16,7 +16,8 @@ export default function PostProjectBtn({}: Props) {
     session?.user.userType === "B" && (
       <Link
         rel="noopener noreferrer"
-        className="inline-flex justify-center items-center gap-1.5 rounded shadow-[0px_4px_20px_0px_rgba(194,194,194,0.40)] text-white text-xl not-italic font-bold leading-[normal] px-2.5 py-1.5 bg-[#0073c6] pr-8 relative"
+        // className="inline-flex justify-center items-center gap-1.5 rounded shadow-[0px_4px_20px_0px_rgba(194,194,194,0.40)] text-white text-xl not-italic font-bold leading-[normal] px-2.5 py-1.5 bg-[#0073c6] pr-8 relative"
+        className="inline-flex justify-center items-center gap-1.5 rounded shadow-[0px_4px_20px_0px_rgba(194,194,194,0.40)] text-white text-[12px] sm:text-[16px] not-italic font-bold leading-[normal] px-2.5 py-1.5 bg-[#0073c6] relative pr-8"
         href={url}
         prefetch={false}
       >
@@ -26,7 +27,7 @@ export default function PostProjectBtn({}: Props) {
           width={36}
           height={36}
           alt="post"
-          className="absolute right-0"
+          className="absolute h-[31px] sm:h-[36] right-0 text-[16px]  "
         />
       </Link>
     )
