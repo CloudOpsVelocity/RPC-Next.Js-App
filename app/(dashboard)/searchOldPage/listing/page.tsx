81e2fc07e81440cb81a957c98a6d6e6576d3d295
dag-bag <virenderkumar23435@gmail.com>
2025-03-19 13:12:38 +0530

    build error solved

--- app/(dashboard)/searchOldPage/listing/page.tsx
+++ app/(dashboard)/searchOldPage/listing/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "@/app/components/layouts/primary/header";
 import SearchAndFilterCon from "./components/searchAndFilterCon";
-import { LeftSideBlock } from "./components/leftSideBlock";
+import LeftSideBlock from "./components/leftSideBlock";
 import LoginPopup from "@/app/components/project/modals/LoginPop";
 import Footer from "@/app/components/layouts/primary/footer";
 import { Toaster } from "react-hot-toast";
