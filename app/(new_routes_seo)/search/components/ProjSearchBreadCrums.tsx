8dab82301e2f58a55f8fa1ed1a88008b5974304f
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2025-04-10 19:08:52 +0530

    Update ProjSearchBreadCrums.tsx

--- app/(new_routes_seo)/search/components/ProjSearchBreadCrums.tsx
+++ app/(new_routes_seo)/search/components/ProjSearchBreadCrums.tsx
@@ -27,7 +27,7 @@ const ProjectSearchBreadCrumbs: React.FC<BreadcrumbProps> = ({ pageUrl }) => {
     newParams = ["Project Search"];
   } else if (pageUrl === "/search/listing") {
     newParams = ["Listing Search"];
-  } else if (pageUrl.includes("/residential/projects/")) {
+  } else if (pageUrl.includes("/residential/")) {
     // trimmedUrl = trimFromWord(pageUrl, "/residential/projects/");
 
     newParams = pageUrl.split("/");
@@ -64,7 +64,6 @@ const ProjectSearchBreadCrumbs: React.FC<BreadcrumbProps> = ({ pageUrl }) => {
       aria-label="Project Search Breadcrumbs"
       className="w-full px-[8px] sm:px-[10px] lg:px-[14px] py-[6px] md:py-[10px] xl:py-4 bg-gray-100 rounded-md shadow-sm max-w-[100%] overflow-x-auto "
     >
-      {JSON.stringify(newParams)}
       <script
         id="ListingBreadCrumbsScript3"
         type="application/ld+json"
@@ -73,7 +72,7 @@ const ProjectSearchBreadCrumbs: React.FC<BreadcrumbProps> = ({ pageUrl }) => {
         }}
       />
       <ol className="flex items-center space-x-1 md:space-x-3  text-sm text-gray-600 pr-[10px] ">
-        <li>
+        {/* <li>
           <Link
             rel="noopener noreferrer"
             href="/"
@@ -82,15 +81,18 @@ const ProjectSearchBreadCrumbs: React.FC<BreadcrumbProps> = ({ pageUrl }) => {
             <FaHome className="h-5 w-5 flex-shrink-0" aria-hidden="true" />
             <span className="sr-only">Home</span>
           </Link>
-        </li>
+        </li> */}
         {newParams.map((item: any, index: number) => {
-          let url = trimStringUrl(pageUrl, item);
+          let url = index === 0 ? "/" : trimStringUrl(pageUrl, item);
           return (
             <li key={item + index.toString()} className="flex items-center">
-              <FaChevronRight
-                className="h-4 w-4 flex-shrink-0 text-gray-400"
-                aria-hidden="true"
-              />
+              {index !== 0 && (
+                <FaChevronRight
+                  className="h-4 w-4 flex-shrink-0 text-gray-400"
+                  aria-hidden="true"
+                />
+              )}
+
               {index !== newParams.length - 1 ? (
                 <Link
                   rel="noopener noreferrer"
@@ -102,13 +104,25 @@ const ProjectSearchBreadCrumbs: React.FC<BreadcrumbProps> = ({ pageUrl }) => {
                     index === newParams.length - 1 ? "page" : undefined
                   }
                 >
-                  {item.replaceAll("-", " ")}
+                  {item ? (
+                    item.replaceAll("-", " ")
+                  ) : (
+                    <FaHome
+                      className="h-5 w-5 flex-shrink-0"
+                      aria-hidden="true"
+                    />
+                  )}
                 </Link>
               ) : (
                 <span
                   className={`ml-2 text-sm font-semibold text-gray-800 hover:text-blue-600 transition-all duration-200 text-nowrap first-letter:capitalize `}
                 >
-                  {item.replaceAll("-", " ")}
+                  {item.replaceAll("-", " ") ?? (
+                    <FaHome
+                      className="h-5 w-5 flex-shrink-0"
+                      aria-hidden="true"
+                    />
+                  )}
                 </span>
               )}
             </li>
