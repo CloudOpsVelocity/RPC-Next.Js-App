13c6d7532b51b201e2a1d85e28c71da9eb501a4b
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2025-04-16 13:55:02 +0530

    ++ DONE

--- app/seo/search/ResidentialProject.shcema.tsx
+++ app/seo/search/ResidentialProject.shcema.tsx
@@ -239,11 +239,6 @@ export const generateAllSchemas = (
           priceCurrency: "INR",
         },
       },
-      {
-        "@type": "ListItem",
-        position: 1,
-        url: "https://example.com/peanut-butter-cookies.html",
-      },
       ...allSizesSchemas,
     ],
   };
@@ -360,27 +355,30 @@ export const ResidentialProjectSchama = ({
   const description = `Discover a wide range of residential properties including apartments, villas, independent houses, and gated communities. Find your perfect home in prime locations with the best amenities and lifestyle features.`;
   const totalResults = totalPages;
 
-  // const itemListSchema = {
-  //   "@context": "https://schema.org",
-  //   "@type": "ItemList",
-  //   itemListElement: [
-  //     {
-  //       "@type": "ListItem",
-  //       position: 1,
-  //       url: "https://example.com/peanut-butter-cookies.html",
-  //     },
-  //     {
-  //       "@type": "ListItem",
-  //       position: 2,
-  //       url: "https://example.com/triple-chocolate-chunk.html",
-  //     },
-  //     {
-  //       "@type": "ListItem",
-  //       position: 3,
-  //       url: "https://example.com/snickerdoodles.html",
-  //     },
-  //   ],
-  // };
+  const itemListSchema = {
+    "@context": "https://schema.org",
+    "@type": "ItemList",
+    itemListElement: properties.map((property, index) => ({
+      "@type": "ListItem",
+      position: index + 1,
+      url: `${process.env.NEXT_PUBLIC_URL}${createProjectLinkUrl({
+        city: property.city || "Bengaluru",
+        slug: property.projName,
+        locality: property.locality || "Whitefield",
+        projIdEnc: property.projIdEnc,
+      })}`,
+    })),
+    totalItems: totalResults, // Total number of items
+    nextPage:
+      page < totalPages
+        ? `${process.env.NEXT_PUBLIC_URL}/residential?page=${page + 1}`
+        : null, // URL for the next page
+    previousPage:
+      page > 1
+        ? `${process.env.NEXT_PUBLIC_URL}/residential?page=${page - 1}`
+        : null, // URL for the previous page
+    currentPage: page, // Current page number
+  };
 
   return (
     <>
@@ -391,12 +389,12 @@ export const ResidentialProjectSchama = ({
           __html: JSON.stringify(viewActionJsonLd),
         }}
       />
-      {/* <script
+      <script
         type="application/ld+json"
         dangerouslySetInnerHTML={{
           __html: JSON.stringify(itemListSchema),
         }}
-      /> */}
+      />
       <script
         type="application/ld+json"
         dangerouslySetInnerHTML={{
