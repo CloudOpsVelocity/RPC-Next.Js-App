ac8f77387b7d3de6000fdda0b07af9397d862dc8
rpc-manish <144668589+rpc-manish@users.noreply.github.com>
2025-04-16 19:24:12 +0530

    commited by manish ++ broucher
    commitd by broucher

--- app/components/property/OverViewBanner.tsx
+++ app/components/property/OverViewBanner.tsx
@@ -69,13 +69,32 @@ export default function PropertyOverviewBanner({
        */}
       
       <Image
-        src={`${process.env.NEXT_PUBLIC_IMG_BASE}/staticmedia-images-icons/project-detail/pricebag.webp`}
-        width={237}
-        height={263}
-        alt="priceBagOverView"
-        className="w-[100px] h-[120px]  sm:w-[151px] xl:w-[237px]  sm:h-[169px] xl:h-[263px] mt-2 sm:mt-0"
-        unoptimized
-      />    
+              src={`${process.env.NEXT_PUBLIC_IMG_BASE}/staticmedia-images-icons/project-detail/pricebag-mobile.webp`}
+              width={100}
+              height={120}
+              alt="price OverView"
+              className="w-[100px] sm:hidden h-[120px] sm:h-[169px] mt-2 sm:mt-0"
+              unoptimized
+              title="price OverView"
+            />
+            <Image
+              src={`${process.env.NEXT_PUBLIC_IMG_BASE}/staticmedia-images-icons/project-detail/pricebag-laptop.webp`}
+              width={151}
+              height={169}
+              alt="price OverView"
+              className=" sm:max-w-[151px] hidden sm:flex xl:hidden  sm:w-[151px]   sm:h-[169px] mt-2 sm:mt-0"
+              unoptimized
+              title="price OverView"
+            />
+            <Image
+              src={`${process.env.NEXT_PUBLIC_IMG_BASE}/staticmedia-images-icons/project-detail/pricebag.webp`}
+              width={237}
+              height={263}
+              alt="price OverView"
+              className="   xl:max-w-[237px]  xl:w-[237px] xl:h-[263px] hidden xl:flex  mt-2 sm:mt-0"
+              unoptimized
+              title="price OverView"
+            />   
       <div className="flex justify-center sm:justify-between items-center w-[100%] flex-row sm:ml-[3%] p-[2%] flex-wrap">
         <div className="flex  flex-col text-left">
           <h4 className="text-[#212C33] sm:text-[24px] xl:text-[34px] font-[600]  md:text-start text-center">
