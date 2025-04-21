bfe96a16d2d392cc3f9d1e0d2c73bf3f8e81bbb6
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2025-04-21 12:20:41 +0530

    Update newcardforlsiting.tsx

--- app/(new_routes_seo)/(project-listing-route)/residential-listings/_components/newcardforlsiting.tsx
+++ app/(new_routes_seo)/(project-listing-route)/residential-listings/_components/newcardforlsiting.tsx
@@ -112,7 +112,13 @@ export function PropertyCard({ property }: any) {
         </span>
         <Image
           src={coverImageUrl || "/placeholder.svg?height=400&width=600"}
-          alt={property.propName}
+          alt={`${property.bhkName} ${property.propTypeName} for ${
+            property.category
+          } in ${property.localityName} - ${
+            property.propName
+          } | Price: ₹${formatPrice(property.price)} | Address: ${
+            property.address || property.localityName
+          }`}
           fill
           className="object-cover"
         />
