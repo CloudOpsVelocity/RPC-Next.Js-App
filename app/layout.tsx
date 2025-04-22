118e17010e1f2d978a9c25346cd414e4dba992ce
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2025-04-22 13:08:58 +0530

    ++ DONE

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,12 +8,13 @@ import { GoogleTagManager } from "@next/third-parties/google";
 import SessionProvider from "./context/session";
 import ReactQueryProvider from "./context/rquery";
 import Layout from "@/app/components/layouts/primary";
-import { Provider } from "jotai";
+import { createStore, Provider } from "jotai";
 import montserrat from "@/font";
 import Header from "./components/layouts/primary/header";
 import Footer from "./components/layouts/primary/footer";
 import { Organization_SCHEMA } from "./seo/common/organisation-details";
 import Script from "next/script";
+import JotaiProvider from "./context/JotaiProvider";
 // const playball = Playball({
 //   subsets: ['latin'], // Specify the subset you need
 //   weight: ['400'], // Required weight for Playball font
@@ -118,11 +119,11 @@ export default function RootLayout(params: { children: React.ReactNode }) {
           <main>
             <SessionProvider>
               <ReactQueryProvider>
-                <Provider>
+                <JotaiProvider>
                   <Header />
                   <Layout>{params.children}</Layout>
                   <Footer />
-                </Provider>
+                </JotaiProvider>
               </ReactQueryProvider>
             </SessionProvider>
           </main>
