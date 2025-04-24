5f901314c9489c48988fa01d98d79de203ae3f62
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2025-04-24 10:57:08 +0530

    Update font.ts

--- font.ts
+++ font.ts
@@ -1,58 +1,60 @@
-import localFont from 'next/font/local'
+import localFont from "next/font/local";
 
 const montserrat = localFont({
-    src: [
-      {
-        path: './app/fonts/Montserrat/static/Montserrat-Regular.ttf',
-        weight: '400',
-        style: 'normal',
-      },
-      {
-        path: './app/fonts/Montserrat/static/Montserrat-Italic.ttf', 
-        weight: '400',
-        style: 'italic',
-      },
-      {
-        path: './app/fonts/Montserrat/static/Montserrat-Bold.ttf',
-        weight: '700',
-        style: 'normal',
-      },
-      {
-        path: './app/fonts/Montserrat/static/Montserrat-BoldItalic.ttf',
-        weight: '700',
-        style: 'italic',
-      },
-      {
-        path: './app/fonts/Montserrat/static/Montserrat-Medium.ttf',
-        weight: '500',
-        style: 'normal',
-      },
-      {
-        path: './app/fonts/Montserrat/static/Montserrat-SemiBold.ttf',
-        weight: '600',
-        style: 'normal',
-      },
-      {
-        path: './app/fonts/Montserrat/static/Montserrat-Light.ttf',
-        weight: '300',
-        style: 'normal',
-      },
-      {
-        path: './app/fonts/Montserrat/static/Montserrat-ExtraLight.ttf',
-        weight: '200',
-        style: 'normal',
-      },
-      {
-        path: './app/fonts/Montserrat/static/Montserrat-ExtraBold.ttf',
-        weight: '800',
-        style: 'normal',
-      },
-      {
-        path: './app/fonts/Montserrat/static/Montserrat-Black.ttf',
-        weight: '900',
-        style: 'normal',
-      }
-    ],
-  })
+  preload: true,
+  adjustFontFallback: false,
+  src: [
+    {
+      path: "./app/fonts/Montserrat/static/Montserrat-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./app/fonts/Montserrat/static/Montserrat-Italic.ttf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "./app/fonts/Montserrat/static/Montserrat-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./app/fonts/Montserrat/static/Montserrat-BoldItalic.ttf",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "./app/fonts/Montserrat/static/Montserrat-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./app/fonts/Montserrat/static/Montserrat-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "./app/fonts/Montserrat/static/Montserrat-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "./app/fonts/Montserrat/static/Montserrat-ExtraLight.ttf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "./app/fonts/Montserrat/static/Montserrat-ExtraBold.ttf",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "./app/fonts/Montserrat/static/Montserrat-Black.ttf",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+});
 
-  export default montserrat
+export default montserrat;
