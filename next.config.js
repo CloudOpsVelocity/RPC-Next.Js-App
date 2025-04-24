25a2ea5e346f607fbf54314fef832ba893dbb6dc
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2025-04-24 10:46:15 +0530

    ++ DDONE

--- next.config.js
+++ next.config.js
@@ -19,7 +19,7 @@ const nextConfig = {
   experimental: {
     optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
     mdxRs: true,
-    optimizeCss: true,
+    // optimizeCss: true,
     // removeConsole: process.env.NODE_ENV === 'production',
   },
   images: {
