f13bfa4caa30883e17de0b478875209553fde9a9
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2025-04-25 14:00:07 +0530

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,19 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    cssnano: { preset: "default" },
-  },
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    require("cssnano")({
+      preset: [
+        "default",
+        {
+          discardComments: { removeAll: true }, // Removes all comments
+          normalizeWhitespace: true, // Minifies whitespace
+          mergeRules: true, // Merges duplicate rules
+          convertValues: true, // Optimizes values (e.g., px → 0)
+          reduceInitial: true, // Compress `initial` values
+          svgo: true, // Optimizes SVGs (if any in CSS)
+        },
+      ],
+    }),
+  ],
 };
