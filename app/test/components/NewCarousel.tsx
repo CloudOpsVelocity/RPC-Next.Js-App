0fd3034f110f72caf22db5cd6dfd57a5fcbcf2e6
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2025-04-25 12:47:45 +0530

    Update NewCarousel.tsx

--- app/test/components/NewCarousel.tsx
+++ app/test/components/NewCarousel.tsx
@@ -81,7 +81,7 @@ function NewCarousel<T>({
       href={url ?? "/"}
       target="_blank"
       // onClick={() => (url ? window.open(url, "_self", "noreferrer") : "")}
-      className="relative h-full w-full rounded-xl bg-gradient-to-br from-blue-400 to-blue-600 p-1 hover:shadow-xl cursor-pointer transition-all duration-300 "
+      className="relative h-full w-full rounded-xl bg-gradient-to-br from-blue-400 to-blue-600  hover:shadow-xl cursor-pointer transition-all duration-300 "
     >
       <div className="h-full w-full rounded-lg bg-white p-4 flex flex-col items-center justify-center space-y-4">
         <div className="rounded-full bg-gradient-to-br from-blue-400 to-blue-600 p-3 animate-pulse">
@@ -133,7 +133,7 @@ function NewCarousel<T>({
     <div className="relative group mt-2">
       <div
         ref={containerRef}
-        className={`relative overflow-x-auto scrollbar-hide`}
+        className={`relative  scrollbar-hide`}
         style={{ margin: isMobile ? 0 : `0 ${gap / 2}px` }}
       >
         <div
