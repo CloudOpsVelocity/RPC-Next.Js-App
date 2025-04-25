6ccd6f69d946d3eb55278e3c1cff3e5a77b1d963
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2025-04-25 13:38:32 +0530

    ++ DONE

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,6 @@
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-});
-
 module.exports = {
-  plugins: [
-    "tailwindcss",
-    "autoprefixer",
-    // ...(process.env.NODE_ENV === "production" ? [purgecss] ),
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
