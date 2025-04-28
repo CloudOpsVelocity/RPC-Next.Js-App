26feb59a09291e7e8a191ebd03febc99e49c98a6
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2025-04-28 17:28:44 +0530

    Update Mainsection.tsx

--- app/(new_routes_seo)/search/components/Mainsection.tsx
+++ app/(new_routes_seo)/search/components/Mainsection.tsx
@@ -34,7 +34,7 @@ export default function Mainsection({ frontendFilters, serverData }: Props) {
         {
           type: "update",
           payload: {
-            // ...initialState,
+            ...initialState,
             ...frontendFilters,
           },
         },
