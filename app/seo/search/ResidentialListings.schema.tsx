f7bc8dae5aa8132a325d98313e783b0025407a6b
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2025-04-28 13:18:51 +0530

    ++ done

--- app/seo/search/ResidentialListings.schema.tsx
+++ app/seo/search/ResidentialListings.schema.tsx
@@ -2,6 +2,8 @@ import { createProjectLinkUrl } from "@/app/utils/linkRouters/ProjectLink";
 import WebPageSchama from "../common/WebPageSchama";
 import { ListingSearchSchema } from "./listing-search.schema";
 import { baseURL } from "@/app/utils/api/api";
+import OrganizationSchema from "../OraganisationSchema";
+import LocalBusinessJsonLdScript from "../Localjson";
 
 type Props = {
   properties: any;
