c0df4aab30689cd6f93ff6cd88b4554bd72fe062
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2025-04-30 19:12:08 +0530

    ++ DONE

--- app/(sitemaps)/dynamic-sitemap/[slug]/route.ts
+++ app/(sitemaps)/dynamic-sitemap/[slug]/route.ts
@@ -4,6 +4,7 @@ import { NextResponse } from "next/server";
 import { getServerSideSitemap } from "next-sitemap";
 import redisService from "@/app/utils/redis/redis.service";
 import { SlugsType } from "@/app/common/constatns/slug.constants";
+import axios from "axios";
 
 // Utility function to split the array into chunks
 const chunkArray = (array: any[], chunkSize: number) => {
@@ -20,29 +21,9 @@ export async function GET(
 ) {
   const slug = (await params).slug; // Get the 'slug' param, which is the index
   const sitemapIndex = parseInt(slug);
-
-  if (isNaN(sitemapIndex) || sitemapIndex < 0) {
-    return NextResponse.json({ message: "Invalid Sitemap Index" });
-  }
-
-  // Path to your JSON file (projectSlugs or case-seo.json)
-  const projectSlugs = await redisService.getSeoSlug(SlugsType.SEO);
-
-  // Set the chunk size (50,000 entries per sitemap)
-  const chunkSize = 30000;
-
-  // Split the project slugs into chunks of 50,000
-  const sitemapChunks = chunkArray(projectSlugs, chunkSize);
-  console.log(sitemapChunks.length);
-  // Check if the requested index is within the valid range
-  if (sitemapIndex >= sitemapChunks.length) {
-    return NextResponse.json({ message: "Sitemap index out of range" });
-  }
-
-  // Get the requested sitemap chunk
-  const requestedSitemap = sitemapChunks[sitemapIndex];
-
-  // Generate the sitemap response for the requested chunk
+  const res = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/common/case-seo-page?size=30000&page=${sitemapIndex}`)
+ const requestedSitemap = res.data.seoUrls
+ 
   const sitemap = requestedSitemap.map((slug: any) => ({
     loc: `${process.env.NEXT_PUBLIC_URL}/${slug}`,
     lastmod: new Date().toISOString(),
