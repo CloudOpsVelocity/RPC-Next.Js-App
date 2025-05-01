c465c7831159488c4a48cddb219521d059a2348c
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2025-05-01 19:03:47 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,6 @@ export default function RootLayout(params: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <Script
           strategy="lazyOnload"
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9243707404617437"
