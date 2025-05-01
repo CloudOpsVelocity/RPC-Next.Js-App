dbfa988fb7541dbb1a74d8e6ab8fc26e964637cd
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2025-05-01 17:01:50 +0530

    ++ FRONT ROWS

--- app/(new_routes_seo)/(project-listing-route)/residential-listings/[cg]/[city]/[lt]/[project]/[phase]/[bhk_unit_type]/page.tsx
+++ app/(new_routes_seo)/(project-listing-route)/residential-listings/[cg]/[city]/[lt]/[project]/[phase]/[bhk_unit_type]/page.tsx
@@ -1,7 +1,8 @@
 import ListingDetailsPage from "@/app/(dashboard)/listing/[city]/[slug]/Page/ListingDetailsPage";
-import { getSearchData } from "@/app/(new_routes_seo)/in/utils/api";
+import { getProjSearchData, getSearchData } from "@/app/(new_routes_seo)/in/utils/api";
 import { findPathForProjectListing } from "@/app/(new_routes_seo)/in/utils/getSlugs";
 import NewListingSearchpage from "@/app/(new_routes_seo)/search/listing/NewListingSearchpage";
+import { parseApiFilterQueryParams } from "@/app/(new_routes_seo)/search/utils/project-search-queryhelpers";
 import { extractListingParamsValues } from "@/app/(new_routes_seo)/utils/new-seo-routes/listing";
 import { BASE_PATH_PROJECT_LISTING } from "@/app/(new_routes_seo)/utils/new-seo-routes/listing.route";
 import { getAmenties, getAuthorityNames } from "@/app/utils/api/project";
@@ -22,9 +23,12 @@ type Props = {
     project: string;
     phase: string;
   };
+  searchParams: {
+    sf: string;
+  };
 };
 
-export default async function Page({ params }: Props) {
+export default async function Page({ params, searchParams }: Props) {
   const { bhk_unit_type, cg, city, lt, project, phase } = params;
   const pathname = `${BASE_PATH_PROJECT_LISTING}/${cg}/${city}/${lt}/${project}${
     phase ? `/${phase}` : ""
@@ -33,63 +37,31 @@ export default async function Page({ params }: Props) {
   let isProjectListing = bhk_unit_type.includes("listing");
   let serverData = null;
   let filtersValues: any = {};
-  // const isProjectListing =
-  // filtersValues.PT == "32"
-  //   ? filtersValues.count === 7
-  //   : filtersValues.count === 8;
-  // console.log(isProjectListing, pathname);
-  if (!isProjectListing) {
-    const values = await findPathForProjectListing(pathname);
-    // console.log(values);
-    if (!values) return notFound();
-    filtersValues = extractListingParamsValues(values);
-    serverData = await getSearchData(
-      `${filtersValues.BH ? `bhk=${filtersValues.BH}&` : ""}propType=${
-        filtersValues.PT
-      }&localities=${filtersValues.LT}&cg=${filtersValues.CG}&projIdEnc=${
-        filtersValues.PJ
-      }${filtersValues.PH ? `&phaseId=${filtersValues.PH}` : ""}`
-    );
-  } else {
-    const {
-      listing: data,
-      nearByLocations,
-      totalPrice,
-    } = await getListingDetails(
-      (bhk_unit_type.split("-").at(-1) as string) ?? "",
-      pathname
-    );
+   let frontendFilters = null
+   if(searchParams.sf){
+    const apiFilters = parseApiFilterQueryParams(searchParams.sf)
+    const isProj = apiFilters?.includes("listedBy=proj") ? true : false;
+    // eslint-disable-next-line no-unused-vars
+    serverData = isProj
+      ? await getProjSearchData(apiFilters ?? "")
+      : await getSearchData(apiFilters ?? "");
 
-    const [projData, issueData, amenities] = await Promise.all([
-      getProjectDetails(data.projIdEnc),
-      getReportConstData(),
-      getAmenties(),
-    ]);
-    if (projData?.projAuthorityId) {
-      const res = await getAuthorityNames(projData.projAuthorityId);
-      data.projAuthorityNames = res;
-    }
-    const TITLE_OF_PROP = data.projIdEnc
-      ? data.propName
-      : `${data.bhkName ?? ""} ${data.propTypeName} For
-    ${data.cg === "S" ? " Sale" : " Rent"} In ${data.ltName}`;
-
-    serverData = {
-      TITLE_OF_PROP,
-      data,
-      projData,
-      issueData,
-      amenitiesFromDB: amenities,
-      nearByLocations,
-      totalPrice,
-    };
+       frontendFilters = parseApiFilterQueryParams(searchParams.sf);
   }
-
-  return !isProjectListing ? (
-    <NewListingSearchpage
-      pageUrl={pathname}
-      serverData={serverData}
-      frontendFilters={{
+  else{
+    if (!isProjectListing) {
+      const values = await findPathForProjectListing(pathname);
+      // console.log(values);
+      if (!values) return notFound();
+      filtersValues = extractListingParamsValues(values);
+      serverData = await getSearchData(
+        `${filtersValues.BH ? `bhk=${filtersValues.BH}&` : ""}propType=${
+          filtersValues.PT
+        }&localities=${filtersValues.LT}&cg=${filtersValues.CG}&projIdEnc=${
+          filtersValues.PJ
+        }${filtersValues.PH ? `&phaseId=${filtersValues.PH}` : ""}`
+      );
+      frontendFilters = {
         localities: [`${lt}+${filtersValues.LT}`],
         bhk: [parseInt(filtersValues.BH as string)],
         propType: parseInt(filtersValues.PT as string),
@@ -98,8 +70,51 @@ export default async function Page({ params }: Props) {
         projIdEnc: filtersValues.PH,
         ...(filtersValues.PH && {
           phaseId: [`${params.phase}+${filtersValues.PH}`],
-        }),
-      }}
+        })
+      }
+    } else {
+      const {
+        listing: data,
+        nearByLocations,
+        totalPrice,
+      } = await getListingDetails(
+        (bhk_unit_type.split("-").at(-1) as string) ?? "",
+        pathname
+      );
+  
+      const [projData, issueData, amenities] = await Promise.all([
+        getProjectDetails(data.projIdEnc),
+        getReportConstData(),
+        getAmenties(),
+      ]);
+      if (projData?.projAuthorityId) {
+        const res = await getAuthorityNames(projData.projAuthorityId);
+        data.projAuthorityNames = res;
+      }
+      const TITLE_OF_PROP = data.projIdEnc
+        ? data.propName
+        : `${data.bhkName ?? ""} ${data.propTypeName} For
+      ${data.cg === "S" ? " Sale" : " Rent"} In ${data.ltName}`;
+  
+      serverData = {
+        TITLE_OF_PROP,
+        data,
+        projData,
+        issueData,
+        amenitiesFromDB: amenities,
+        nearByLocations,
+        totalPrice,
+      };
+    }
+  }
+  
+
+  return !isProjectListing ? (
+    <NewListingSearchpage
+      pageUrl={pathname}
+      serverData={serverData}
+       frontendFilters={frontendFilters}
+       preDefinedFilters={searchParams.sf}
     />
   ) : (
     <ListingDetailsPage params={params} {...serverData} pathname={pathname} />
