9edb1ae74f46e087d4d1c706e21584e1883d10eb
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2025-05-06 12:48:37 +0530

    ++ DONE

--- app/test/newui/page.tsx
+++ app/test/newui/page.tsx
@@ -1,52 +1,25 @@
-import { unstable_getImgProps as getImageProps } from "next/image";
+import React from "react";
 
-export default function Home() {
-  const common = { alt: "Art Direction Example", sizes: "100vw" };
+type Props = {};
 
-  const {
-    props: { srcSet: desktopSrcSet },
-  } = getImageProps({
-    ...common,
-    width: 1440,
-    height: 875,
-    quality: 80,
-    src: "https://media.getrightproperty.com/residential-projects/bengaluru/1181/affinity-sarovar-avalahalli-cover-medium.webp?v=1745586658282",
+export default function page({}: Props) {
+  class Device {
+    static device: "mobile" | "desktop" = "mobile";
+  }
+  class DevicePlatForm extends Device {
+    static platform: "ios" | "android" = "android";
+  }
+  class Laptop extends DevicePlatForm {
+    private battery: string;
+    constructor({ battery }: { battery: string }) {
+      super();
+      this.battery = battery;
+    }
+  }
+  const laptop = new Laptop({
+    battery: "80vh",
   });
+  console.log(laptop);
 
-  const {
-    props: { srcSet: mobileSrcSet },
-  } = getImageProps({
-    ...common,
-    width: 750,
-    height: 1334,
-    quality: 70,
-    src: "https://media.getrightproperty.com/residential-projects/bengaluru/1181/affinity-sarovar-avalahalli-cover-small.webp?v=1745586658282",
-  });
-
-  // Use one of them (e.g., desktop) as fallback img props
-  const {
-    props: { src, ...imgProps },
-  } = getImageProps({
-    ...common,
-    width: 1440,
-    height: 875,
-    quality: 80,
-    src: "https://media.getrightproperty.com/residential-projects/bengaluru/1181/affinity-sarovar-avalahalli-cover-large.webp?v=1745586658282",
-  });
-
-  return (
-    <div className="mt-[10%]">
-      <picture>
-        {JSON.stringify(desktopSrcSet)}
-        <source media="(min-width: 768px)" srcSet={desktopSrcSet} />
-        <source media="(max-width: 767px)" srcSet={mobileSrcSet} />
-        <img
-          src={src}
-          {...imgProps}
-          style={{ width: "100%", height: "auto" }}
-          alt={common.alt}
-        />
-      </picture>
-    </div>
-  );
+  return <div className="mt-[10%]">{JSON.stringify(laptop)}</div>;
 }
