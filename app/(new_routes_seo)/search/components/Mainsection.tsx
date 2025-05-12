0b8671c5f744c5d60a98518c4cf15c017730335e
rpc-vinay <131428813+rpc-vinay@users.noreply.github.com>
2025-05-12 18:01:00 +0530

    latest code

--- app/(new_routes_seo)/search/components/Mainsection.tsx
+++ app/(new_routes_seo)/search/components/Mainsection.tsx
@@ -91,7 +91,7 @@ export default function Mainsection({
         isMobile !== undefined &&
         isMobile === false && (
           <div
-            className={`relative w-full max-h-[70vh] sm:fixed right-0 flex justify-center items-center md:w-[60%] xl:w-[50%] scroll-mt-[150px] z-0 border-[2px] border-black-500 border-solid h-[calc(100vh-65vh)] md:h-[calc(100vh-255px)] max-w-full`}
+            className={`relative w-full max-h-[70vh] sm:fixed right-0 flex justify-center items-center md:w-[50%] scroll-mt-[150px] z-0 border-[2px] border-black-500 border-solid h-[calc(100vh-65vh)] md:h-[calc(100vh-255px)] max-w-full`}
           >
             <Image
               priority
