b2a037f3303f7041fa6c5cdc55bda1e1a39577cc
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2025-05-14 18:25:05 +0530

    Update page.tsx

--- app/test/newui/page.tsx
+++ app/test/newui/page.tsx
@@ -5,6 +5,16 @@ const EventBubblingThreeContainers: React.FC = () => {
   const handleWrapperClick = (
     e: React.MouseEvent<HTMLDivElement, MouseEvent>
   ) => {
+    const target = e.target as HTMLElement;
+    const card = target.closest("[data-id]");
+    const container = target.closest("[container-data-id]");
+    if (card) {
+      const data_id = card.getAttribute("data-id");
+      alert("clicked card id is " + data_id);
+    } else if (container) {
+      const data_id = container.getAttribute("container-data-id");
+      alert("clicked Container id is " + data_id);
+    }
     console.log("%cWrapper clicked", "color: purple; font-weight: bold");
   };
 
@@ -29,13 +39,13 @@ const EventBubblingThreeContainers: React.FC = () => {
 
   return (
     <div
-      // onClick={()}
+      onClick={(e) => handleWrapperClick(e)}
       className="p-8 bg-purple-100 min-h-screen flex justify-center gap-8 pt-[10%]"
     >
       {[1, 2, 3].map((id) => (
         <div
           key={id}
-          data-id={`container-${id}`}
+          card-data-id={`container-${id}`}
           // onClickCapture={(e) => handleContainerClick(id, e)}
           className="p-6 bg-green-100 rounded-lg shadow-lg"
         >
