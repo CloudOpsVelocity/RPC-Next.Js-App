6e8f75c8450d985dfdea6a957cd1bbe433ca38a2
rpc-vinay <131428813+rpc-vinay@users.noreply.github.com>
2025-05-20 19:56:24 +0530

    working with performance

--- app/(new_routes_seo)/search/_new-search-page/components/Mainsection.tsx
+++ app/(new_routes_seo)/search/_new-search-page/components/Mainsection.tsx
@@ -1,10 +1,10 @@
 "use client";
-import { useHydrateAtoms } from "jotai/utils";
+// import { useHydrateAtoms } from "jotai/utils";
 import dynamic from "next/dynamic";
 import { useQueryState } from "nuqs";
 import React, { useState } from "react";
 import {
-  projSearchStore,
+  // projSearchStore,
   searchPageMapToggle,
 } from "../../store/newSearchProjectStore";
 import Image from "next/image";
@@ -74,7 +74,7 @@ export default function Mainsection({
         apiFilterQueryParams={apiFilterQueryParams}
         preAppliedFilters={preAppliedFilters}
       />
-      <div className="w-[100%] sm:w-[50%] -z-10" />
+      {/* <div className="w-[100%] sm:w-[50%] -z-10" /> */}
 
       {isMapLoaded || isMobile ? (
         <RightSection
@@ -85,40 +85,56 @@ export default function Mainsection({
       ) : (
         isMobile !== undefined &&
         isMobile === false && (
-          <div
-            className={`hidden md:flex relative w-full max-h-[70vh] sm:fixed right-0 flex justify-center items-center md:w-[50%] scroll-mt-[150px] z-0 border-[2px] border-black-500 border-solid h-[calc(100vh-65vh)] md:h-[calc(100vh-255px)] max-w-full`}
-          >
-            <Image
-              priority
-              height={630}
-              width={1200}
-              src={`${process.env.NEXT_PUBLIC_IMG_BASE}/staticmedia-images-icons/search-page/default-search-page-map.webp`}
-              alt="search page map Image"
-              className="h-full w-full"
-              quality={80}
-            />
-{/* 
-            <Image
-              priority
-              src={`${process.env.NEXT_PUBLIC_IMG_BASE}/staticmedia-images-icons/search-page/default-search-page-map.webp`}
-              alt="search page map Image"
-              fill
-              className="object-contain" // or "object-cover", depending on your layout needs
-              // quality={80}
-            /> */}
+               <div
+                className="hidden md:flex relative w-full md:w-[50%] max-w-full sm:fixed right-0 z-0 border-2 border-black border-solid justify-center items-center overflow-hidden max-h-[70vh] h-full aspect-[831/690]"
+              >
+                <Image
+                  priority
+                  src={`${process.env.NEXT_PUBLIC_IMG_BASE}/staticmedia-images-icons/search-page/default-search-page-map.webp`}
+                  alt="Search page map Image"
+                  fill
+                  quality={80}
+                  className="object-contain"
+                  // className="h-full w-full"
+                />
 
-            <button
-              aria-label="Click to View Location Details"
-              name="Click to View Location Details"
-              title="Click to View Location Details"
-              onClick={() => setIsMapLoaded(true)}
-              className="absolute z-8 px-6 py-3 text-white rounded-lg bg-btnPrimary shadow-lg hover:bg-btnPrimary transition-colors "
-            >
-              <span className="text-lg font-semibold">
-                Click to View Location Details
-              </span>
-            </button>
-          </div>
+                <button
+                  aria-label="Click to View Location Details"
+                  title="Click to View Location Details"
+                  onClick={() => setIsMapLoaded(true)}
+                  className="absolute z-10 px-6 py-3 text-white rounded-lg bg-btnPrimary shadow-lg hover:bg-btnPrimary transition-colors"
+                >
+                  <span className="text-lg font-semibold">Click to View Location Details</span>
+                </button>
+              </div>
+
+
+
+          // <div
+          //   className={`hidden md:flex relative w-full !max-h-[70vh] sm:fixed right-0 flex justify-center items-center md:w-[50%] scroll-mt-[150px] z-0 border-[2px] border-black-500 border-solid h-[calc(100vh-65vh)] md:h-[calc(100vh-255px)] max-w-full`}
+          // >
+          //   <Image
+          //     priority
+          //     height={630}
+          //     width={1200}
+          //     src={`${process.env.NEXT_PUBLIC_IMG_BASE}/staticmedia-images-icons/search-page/default-search-page-map.webp`}
+          //     alt="search page map Image"
+          //     className="h-full w-full !max-h-[100vh]"
+          //     quality={80}
+          //   />
+
+          //   <button
+          //     aria-label="Click to View Location Details"
+          //     name="Click to View Location Details"
+          //     title="Click to View Location Details"
+          //     onClick={() => setIsMapLoaded(true)}
+          //     className="absolute z-8 px-6 py-3 text-white rounded-lg bg-btnPrimary shadow-lg hover:bg-btnPrimary transition-colors "
+          //   >
+          //     <span className="text-lg font-semibold">
+          //       Click to View Location Details
+          //     </span>
+          //   </button>
+          // </div>
         )
       )}
     </>
