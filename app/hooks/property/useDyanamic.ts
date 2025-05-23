284b5a6591a12ce30bfb03f1ea701c991a76e983
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2025-05-23 19:07:45 +0530

    Update useDyanamic.ts

--- app/hooks/property/useDyanamic.ts
+++ app/hooks/property/useDyanamic.ts
@@ -11,14 +11,14 @@ export default function useDynamicProp({
   propId: number;
 }) {
   const { data: Session } = useSession();
-  const { slug, bhk_unit_type } = useParams<{
-    slug: string;
-    bhk_unit_type: string;
-  }>();
-
+  const { slugs } = useParams();
   const getData = async () => {
     const res = await axios.get(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/fetch/dynamic-data?propIdEnc=${(slug || bhk_unit_type).split("-")[1]}&category=${cg}&propType=${propId}`
+      `${
+        process.env.NEXT_PUBLIC_BACKEND_URL
+      }/api/v1/fetch/dynamic-data?propIdEnc=${
+        slugs?.at(-1)?.split("-")[1] ?? ""
+      }&category=${cg}&propType=${propId}`
     );
     return res.data;
     // return {
@@ -32,7 +32,7 @@ export default function useDynamicProp({
   };
   const { data, isLoading } = useQuery({
     queryFn: getData,
-    queryKey: ["dynamicproj", slug || bhk_unit_type],
+    queryKey: ["dynamicproj", slugs?.at(-1)?.split("-")[1] ?? ""],
     // staleTime: 30000,
     // refetchOnWindowFocus: false,
     // cacheTime: 30000,
@@ -49,18 +49,26 @@ export default function useDynamicProp({
     onMutate: async (data?: number) => {
       // Cancel any outgoing refetches
       // (so they don't overwrite our optimistic update)
-      await queryClient.cancelQueries({ queryKey: ["dynamicproj", slug || bhk_unit_type] });
-      // Snapshot the previous value
-      const previousTodos = queryClient.getQueryData(["dynamicproj", slug || bhk_unit_type]);
-      // Optimistically update to the new value
-      queryClient.setQueryData(["dynamicproj", slug || bhk_unit_type], (old: any) => {
-        return {
-          ...old,
-          ...(data === 2
-            ? { shortListed: old.shortListed === "Y" ? null : "Y" }
-            : { compareAdded: old.compareAdded === "Y" ? null : "Y" }),
-        };
+      await queryClient.cancelQueries({
+        queryKey: ["dynamicproj", slugs?.at(-1)?.split("-")[1] ?? ""],
       });
+      // Snapshot the previous value
+      const previousTodos = queryClient.getQueryData([
+        "dynamicproj",
+        slugs?.at(-1)?.split("-")[1] ?? "",
+      ]);
+      // Optimistically update to the new value
+      queryClient.setQueryData(
+        ["dynamicproj", slugs?.at(-1)?.split("-")[1] ?? ""],
+        (old: any) => {
+          return {
+            ...old,
+            ...(data === 2
+              ? { shortListed: old.shortListed === "Y" ? null : "Y" }
+              : { compareAdded: old.compareAdded === "Y" ? null : "Y" }),
+          };
+        }
+      );
       return { previousTodos };
     },
 
@@ -75,17 +83,25 @@ export default function useDynamicProp({
     onMutate: async (data?: { rating?: string; review?: string }) => {
       // Cancel any outgoing refetches
       // (so they don't overwrite our optimistic update)
-      await queryClient.cancelQueries({ queryKey: ["dynamicproj", slug || bhk_unit_type] });
-      // Snapshot the previous value
-      const previousTodos = queryClient.getQueryData(["dynamicproj", slug || bhk_unit_type]);
-      // Optimistically update to the new value
-      queryClient.setQueryData(["dynamicproj", slug || bhk_unit_type], (old: any) => {
-        return {
-          ...old,
-          ...(data?.rating === "Y" ? { userRating: "Y" } : null),
-          ...(data?.review === "Y" ? { userReview: "Y" } : null),
-        };
+      await queryClient.cancelQueries({
+        queryKey: ["dynamicproj", slugs?.at(-1)?.split("-")[1] ?? ""],
       });
+      // Snapshot the previous value
+      const previousTodos = queryClient.getQueryData([
+        "dynamicproj",
+        slugs?.at(-1)?.split("-")[1] ?? "",
+      ]);
+      // Optimistically update to the new value
+      queryClient.setQueryData(
+        ["dynamicproj", slugs?.at(-1)?.split("-")[1] ?? ""],
+        (old: any) => {
+          return {
+            ...old,
+            ...(data?.rating === "Y" ? { userRating: "Y" } : null),
+            ...(data?.review === "Y" ? { userReview: "Y" } : null),
+          };
+        }
+      );
       return { previousTodos };
     },
 
