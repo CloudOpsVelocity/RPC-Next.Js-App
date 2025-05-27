b4a065d740eb8596623af029e4a3c3bfd867c7f9
rpc-virender <154510664+rpc-virender@users.noreply.github.com>
2025-05-27 17:14:17 +0530

    Update Tag.tsx

--- app/components/atoms/Tag.tsx
+++ app/components/atoms/Tag.tsx
@@ -8,12 +8,12 @@ export default function Tag({ title, url, className }: TagData) {
       href={url}
       aria-label={title}
       title={title}
-      className="inline-block w-auto"
+      className="inline-block w-auto max-w-[100%] overflow-hidden text-ellipsis"
       prefetch={false}
     >
       <span
         className={` bg-blue-50 text-blue-700 hover:bg-blue-100 transition-colors duration-200 text-xs font-medium rounded-full shadow-sm border border-blue-200
-          !text-[8px] !px-2 !py-[2px] break-words whitespace-nowrap sm:text-sm sm:px-4 sm:py-2 sm:mr-2 sm:mb-2 max-w-[60ch] overflow-hidden text-ellipsis  ${className}`}
+          !text-[8px] !px-2 !py-[2px] break-words whitespace-nowrap sm:text-sm sm:px-4 sm:py-2 sm:mr-2 sm:mb-2   ${className}`}
       >
         {title}
       </span>
